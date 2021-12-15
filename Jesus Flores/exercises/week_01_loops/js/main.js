cf6d2a6184fd0eef09351bf6a4468d2c0759cfd0
Jesus Flores <jesus@Jesuss-MacBook-Pro.local>
2021-12-15 16:31:01 +1100

    Looping around

---
+++ Jesus Flores/exercises/week_01_loops/js/main.js
@@ -0,0 +1,26 @@
+/* Write a for loop that will iterate from 0 to 20. For each iteration, 
+it will check if the current number is even or odd, and report that to the screen (e.g. "2 is even"). */
+
+/* const oddEven = function(number){
+    for (let i = 0; i <= number; i++) {
+        console.log(i % 2 === 0 ? `${i} Even`:`${i} Odd`);
+    }
+};
+oddEven(20);
+ */
+        
+        
+/* Multiplication Tables    
+Write a for loop that will iterate from 0 to 10. For each iteration of the for loop, 
+it will multiply the number by 9 and log the result (e.g. "2 * 9 = 18"). */
+/* 
+const multiplication = function(number){
+    for (let i = 0; i <= number; i++) {
+        console.log(9 * i);
+        for (let j = 1; j <= 10; j++) {
+            console.log(i * j);            
+        }
+    }
+}
+multiplication(10);
+ */
