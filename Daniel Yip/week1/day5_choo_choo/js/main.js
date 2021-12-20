889a4d4c5d17a3b692c2f856dcebe09dcbfea896
danyip <95078748+danyip@users.noreply.github.com>
2021-12-20 23:37:37 +1100

    MTA v1, first attempt...

--- Daniel Yip/week1/day5_choo_choo/js/main.js
+++ Daniel Yip/week1/day5_choo_choo/js/main.js
@@ -1,20 +1,103 @@
-console.log('hello')
+// console.log('hi')
+
+const lineN = ['Times Square', '34th', '28th', '23rd', 'Union Square', '8th'];
+const lineL = ['8th', '6th', 'Union Square', '3rd', '1st'];
+const line6 = ["Grand Central", "33rd", "28th", "23rd", 'Union Square', "Astor Place"];
+
+// CHECKS THE TYPE OF TRIP AND RUNS THE APPROPRIATE FUNCTION
+const planTrip = function(lineOn, stationOn, lineOff, stationOff){
+    let stopCount
+    let totalTripArray
+
+    // check if trip is a single line journey
+    if (lineOn === lineOff){
+        totalTripArray = singleLineTrip(lineOn, stationOn, stationOff);
+        stopCount = totalTripArray.length;
+
+        console.log(`    
+        It is ${stopCount} stops to get from ${stationOn} to ${stationOff}.
+        You will travel through ${totalTripArray}`)
+
+    } else{
+        totalTripArray = multiLineTrip(lineOn, stationOn, lineOff, stationOff);
+
+        stopCount = totalTripArray.length;
+
+        console.log(`
+        It is ${stopCount} stops to get from ${stationOn} to ${stationOff}.
+        You will have to change at Union Square.
+        You will travel through ${totalTripArray.join(', ')}`)
+    }
+
+} //planTrip()
 
 
-const nLine = ['Times Square', '34th', '28th', '23rd', 'Union Square', '8th']
+// RETURNS AN ARRAY WITH A SINGLE LINE TRIP IN THE CORRECT ORDER
+const singleLineTrip = function(line, stationOn, stationOff){
 
-// return array index for UNION SQUARE on selected line
-const nlineInterchange = nLine.indexOf('Union Square');
+    let lineArray = line.slice();
 
-console.log(`index of N interchange ${nlineInterchange}`)
+    let stationOnIndex = stationIndex(stationOn, line)
 
-// return array index for another station on selected line
+    let stationOffIndex = stationIndex(stationOff, line)
 
-const findStationIndex = function(){
+    if (stationOnIndex > stationOffIndex){
+        lineArray.reverse()
+        stationOnIndex = stationIndex(stationOn, lineArray)
+        stationOffIndex = stationIndex(stationOff, lineArray)
+    }
+
+    lineArray = lineArray.slice(stationOnIndex, stationOffIndex);
+
+    return lineArray;
+}
+
+// RETURNS A SINGLE ARRAY WITH A MULTI LINE TRIP IN THE CORRECT ORDER
+const multiLineTrip = function(lineOn, stationOn, lineOff, stationOff){
+    
+    const legOneArray = singleLineTrip(lineOn, stationOn, 'Union Square');
+    
+    const legTwoArray = singleLineTrip(lineOff, 'Union Square', stationOff);
+
+    return legOneArray.concat(legTwoArray);
+}
+
+//RETURN ARRAY INDEX FOR A STATION
+const stationIndex = function(station, line){
+
+    const index = line.indexOf(station);
+
+    return index
 
 } //findStationIndex()
 
 
+// // RETURN ARRAY INDEX FOR 'UNION SQUARE' INTERCHANGE
+// const interchangeIndex = function(line){
+
+//     const index = line.indexOf('Union Square');
+
+//     console.log(`The index of Union Square is ${index}`);
+
+//     return index
+
+// } // interchangeIndex()
 
 
-// count positions between
+// // RETURN THE DISTANCE BETWEEEN STATIONS
+// const distanceBetweenStations = function(firstStation, secondStation){
+
+//     let distance = firstStation - secondStation;
+
+//     if (distance < 0) {
+//         distance = distance * -1;     
+//     }
+
+//     return distance;
+// };  //distanceBetweenStations()
+
+
+planTrip(lineN, '34th', line6, 'Astor Place');
+
+planTrip(lineN, '34th', lineN, 'Times Square');
+
