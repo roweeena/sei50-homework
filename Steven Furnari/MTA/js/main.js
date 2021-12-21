568497cac5a6e57f2b717f11769ac171ec85e479
sfurnari <95397952+sfurnari@users.noreply.github.com>
2021-12-21 14:21:15 +1100

    Add files via upload
    mta homework final

--- Steven Furnari/MTA/js/main.js
+++ Steven Furnari/MTA/js/main.js
@@ -23,7 +23,7 @@ const mta = {
         `1st`,
     ],
     
-    startLineCheck: function(startLine, startStation, endLine, endStation){
+    startLineCheck: function(startLine){
         if (startLine === `6`){
             return this.sixLine
         } else if (startLine === `N`){
@@ -33,7 +33,7 @@ const mta = {
         }            
     }, // startLineCheck() ==> returns correct starting line depending on input
 
-    endLineCheck: function(startLine, startStation, endLine, endStation){
+    endLineCheck: function(endLine){
         if (endLine === `6`){
             return this.sixLine
         } else if (endLine === `N`){
@@ -43,33 +43,59 @@ const mta = {
         }            
     }, // secondLineCheck() ===> returns correct end line depending on input
 
-    startStationIndex: function(startLine, startStation, endLine, endStation){
+    startStationIndex: function(startLine, startStation){
         const line = this.startLineCheck(startLine);
         return line.indexOf(startStation)
-    }, // startStationIndex() 
+    }, // startStationIndex() ===> returns index of the starting station
 
-    endStationIndex: function(startLine, startStation, endLine, endStation){
-        const line = this.endLineCheck(startLine, startStation, endLine, endStation);
+    endStationIndex: function(endLine, endStation){
+        const line = this.endLineCheck(endLine);
         return line.indexOf(endStation)
-    }, // endStationIndex() 
+    }, // endStationIndex() ===> returns index of the end station
 
-    stopCounter: function(startLine, startStation, endLine, endStation){
+    planTrip: function(startLine, startStation, endLine, endStation){
         const start = this.startStationIndex(startLine, startStation);
-        const end = this.endStationIndex(startLine, startStation, endLine, endStation)
+        const end = this.endStationIndex(endLine, endStation);
+        const unionSquareStart = this.startStationIndex(startLine, `Union Square`);
+        const unionSquareEnd = this.endStationIndex(endLine, `Union Square`);
         let stops = 0;
         let trip = [];
-        for (let i = start+1; i < (end+1); i++){
-            stops++;
-            trip.push(this.startLineCheck(startLine)[i])
-        } // for loop
-        return `You will have ${(stops)} stops on your journey, starting at ${startStation} and stopping at following stations: ${trip.join(`, `)}`
-    }, // stopCounter()
-
-
-
-
-
-    // planTrip: function(startLine, startStation, endLine, endStation){
-
-    // planTrip()
-}
+        if (startLine === endLine){ // if trip is on the same line
+            for (let i = start+1; i < end+1; i++){
+                stops++;
+                trip.push(this.startLineCheck(startLine)[i]);
+            }; // going forward
+            for (let i = start-1; i > end-1; i--){
+                stops++;
+                trip.push(this.startLineCheck(startLine)[i]);
+            }; // going back
+            console.log( `You will have ${(stops)} stops on your journey.`);
+            console.log( `Starting at ${startStation} and stopping at following stations: ${trip.join(`, `)}` );
+        } else if (startLine !== endLine) { // trip swaps line at Union Square
+            for (let i = start+1; i < unionSquareStart+1; i++){
+                stops++;
+                trip.push(this.startLineCheck(startLine)[i]);
+            }; // counts stops to union square going forward
+            for (let i = start-1; i > unionSquareStart-1; i--){
+                stops++;
+                trip.push(this.startLineCheck(startLine)[i]);
+            }; // counts stops to union square going back
+            for (let i = unionSquareEnd+1 ; i < end+1; i++){
+                stops++;               
+                trip.push(this.endLineCheck(endLine)[i]);
+            }; // counts stops on new line from union square going forward
+            for (let i = unionSquareEnd-1 ; i > end-1; i--){
+                stops++;               
+                trip.push(this.endLineCheck(endLine)[i]); 
+            }; // counts stops on new line from union square going back
+            console.log( `You will have ${(stops)} stops on your journey.`);
+            console.log( `Starting at ${startStation}, with a change at Union Square to the ${endLine} line, stopping at following stations: ${trip.join(`, `)}` ); 
+        };
+    }, // planTrip()
+}; // mta{}
+mta.planTrip(`6`, `Grand Central`, '6', `Astor Place`) // going forward on single line
+mta.planTrip(`6`, `Astor Place`, '6', `Grand Central`) // going back on single line
+mta.planTrip(`N`, `Times Square`, 'L', `1st`) // going forward with a change
+mta.planTrip(`L`, `1st`, '6', `Grand Central`) // going back with a change
+mta.planTrip(`N`, `Times Square`, '6', `Grand Central`) // going forward and changing to back
+mta.planTrip(`L`, `1st`, '6', `Astor Place`) // going back and changing to forward
