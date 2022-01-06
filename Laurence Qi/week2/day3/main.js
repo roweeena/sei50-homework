b81651a1b7d28f45c4627b379fab813e0e0db4fc
Laurence Qi <laurence.qi@outlook.com>
2022-01-06 23:33:29 +1100

    refactored code

--- Laurence Qi/week2/day3/main.js
+++ Laurence Qi/week2/day3/main.js
@@ -1,64 +1,50 @@
-// generate cat elements from 0 to 199
+// generate cat elements from 0 to 199, with minor differences in 
 for (let i = 0; i < 30; i++) {
     const $currentCat = $('<img src="http://www.anniemation.com/clip_art/images/cat-walk.gif">');
     $currentCat.attr('id', 'cat' + i);
     $currentCat.css({
-        left: i * 10 + 'px',
-        top: i * 30 + 'px',
-        position: 'absolute'
-    })
+            left: i * 10 + 'px',
+            top: i * 30 - 20 + 'px', //minus 20 is to get the top cat closer to the height of the frame
+            position: 'absolute'
+        }) //TODO: Is there a way to generate a custom property that doesn't have to affect the actual DOM node
 
     $('body').append($currentCat);
 }
 
+const screenWidth = globalThis.screen.availWidth;
 const $cat = $('img');
-const walkingRights = new Array($cat.length).fill(true);
-const speeds = new Array($cat.length);
-for (let i = 0; i < $cat.length; i++) {
-    speeds[i] = i + 1;
+const isWalkingRights = new Array($cat.length).fill(true); //TODO: suggest a better name for this. 
+
+const speeds = generateSpeeds();
+
+function generateSpeeds() {
+    currentSpeeds = new Array($cat.length);
+
+    for (let i = 0; i < $cat.length; i++) {
+        currentSpeeds[i] = i + 1;
+    }
+
+    return currentSpeeds
 }
 
-console.log('The $cat object is as follows:', $cat);
-
-console.log('the $cat length is:', $cat.length);
-
-const screenWidth = globalThis.screen.availWidth;
-const screenHeight = globalThis.screen.availHeight;
-
-// console.log("the screen width should be:", screenWidth);
-
-function walk($specificCat, speed) { //direction needs to be positive or negative. 
-    const moveIncrement = speed;
+//TODO: Should I put my functions at the top or the bottom. 
+function walk($specificCat, speed) { //Speed can be positive or negative 
     const currentTopOffset = parseInt($specificCat.css('left'));
-    const newTopOffset = currentTopOffset + moveIncrement;
+    const newTopOffset = currentTopOffset + speed;
     $specificCat.css('left', newTopOffset + 'px');
 }
 
 function walkRightThenLeft() { // TODO: abstract this to do an each for the individual cats to dictate if they are individually going left or right
     // console.log(`cat.left = ${parseInt(cat1.style.left)} & availWIdth ${screenWidth}`);
     $cat.each(function(i) {
-        if (walkingRights[i]) {
-            walk($(this), speeds[i]);
-        } else {
-            walk($(this), -1 * speeds[i]);
-        }
-
+        isWalkingRights[i] ? walk($(this), speeds[i]) : walk($(this), -1 * speeds[i]);
 
         if (parseInt($(this).css('left')) > screenWidth - 255) {
-            walkingRights[i] = false;
+            isWalkingRights[i] = false;
         } else if (parseInt($(this).css('left')) < 0) {
-            walkingRights[i] = true;
-            console.log('changed walking right to true')
+            isWalkingRights[i] = true;
         }
     })
 }
 
-// need to perform the walkRightThenLeft on the .each property. 
-
 setInterval(walkRightThenLeft, 20);
-//create new cat
-
-// when the cat hits the right hand side of the screen then flip image and walk back
-
-
-// Populate this page with several cats. of differing sizes and varieties whenever a cat hits the wall.
