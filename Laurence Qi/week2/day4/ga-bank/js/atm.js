ac075bddd1872a30a28e46d8fcee5c0bf8bd6ad3
Laurence Qi <laurence.qi@outlook.com>
2022-01-07 23:30:03 +1100

    Added main functionality of Account constructor

--- Laurence Qi/week2/day4/ga-bank/js/atm.js
+++ Laurence Qi/week2/day4/ga-bank/js/atm.js
@@ -1,26 +1,37 @@
 // DO the backend bank code first. 
-
-
 // Do some testing with it, then after get the values generated from the bank backend to bind to the visual elements.
 
-const depositing = function(amount, account) {
-
-}
-
-function Account(accType) { //TODO: initialise this as a constructor initially then convert to class. 
-    this.balance = 0;
+function Account(accType, balance = 0) { //TODO: initialise this as a constructor initially then convert to class. 
     this.accType = accType; // just a string
+    this.balance = balance;
     this.deposit = function(amount) {
         this.balance += amount;
     }
-    this.withdraw = function(amount) {
+    this.withdraw = function(amount) { //TODO: figure out how you can get this account to reference the other's balance when attempting for overdrafting
         if (this.balance >= amount) {
             this.balance -= amount;
         } else {
-            console.log('Can not withdraw, current balance is too low :(');
+            // attempting overdraft
+            otherAccCanOverDraft(amount) ? this.balance -= balance : console.log('Can not withdraw, current balance is too low :(');
         }
+    }
+    this.zeroBalance = function() { //only use this function when needing to overdraft. 
+
     }
 }
 
+function otherAccCanOverDraft(amount) {
+    return amount <= allAcc[0].balance + allAcc[1].balance; //TODO: rework the sum account balances.
+}
+
+const checkingAcc = new Account('checking');
+const savingAcc = new Account('saving');
+const allAcc = [checkingAcc, savingAcc];
+
+// I think I need to make a person object and add the actual accounts to their key values pairs, upon which point I can loop 
+
+module.exports = Account;
+
+//TODO: determine if I should be exporting the instance of the object or the actual function and generate a copy there. 
 
 //FRONT END: Capable of entering amount then withdrawing/depositing it / which account
---
+++ Laurence Qi/week2/day4/ga-bank/js/atm.test.js
@@ -0,0 +1,21 @@
+const Account = require('./atm');
+
+const accountInstance = new Account('checking');
+
+
+test('check whether the initialised bank constructor can deposit', () => {
+    accountInstance.deposit(100);
+    expect(accountInstance.balance).toBe(100);
+});
+
+test('check whether Account withdrawal works when balance > withdrawal amount', () => {
+    accountInstance.withdraw(100);
+    expect(accountInstance.balance).toBe(0);
+});
+
+test('check whether Account does not withdraw when balance < withdrawal amount', () => {
+    accountInstance.withdraw(100);
+    expect(accountInstance.balance).toBe(0);
+});
+
+//TODO: figure out a better way of checking capability of overdrafting.
