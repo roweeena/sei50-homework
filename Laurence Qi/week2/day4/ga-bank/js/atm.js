17b2812b6d7bf3d03a417cd77ac4f8b6fe92ea9c
Laurence Qi <laurence.qi@outlook.com>
2022-01-07 23:53:40 +1100

    added zeroBalance & comments

--- Laurence Qi/week2/day4/ga-bank/js/atm.js
+++ Laurence Qi/week2/day4/ga-bank/js/atm.js
@@ -7,17 +7,19 @@ function Account(accType, balance = 0) { //TODO: initialise this as a constructo
     this.deposit = function(amount) {
         this.balance += amount;
     }
-    this.withdraw = function(amount) { //TODO: figure out how you can get this account to reference the other's balance when attempting for overdrafting
+    this.withdraw = function(amount) { //TODO: figure out how to check other account balances. MAYBE HAVE BANK NUMBERS AND A PROPERTY IN THIS CONSTRUCTOR THAT IS ARR WITH LINKS TO OTHER ACCOUNTS
         if (this.balance >= amount) {
             this.balance -= amount;
         } else {
             // attempting overdraft
             otherAccCanOverDraft(amount) ? this.balance -= balance : console.log('Can not withdraw, current balance is too low :(');
+            //TODO: remove the money from one of the other accounts and zero the balance here. 
         }
     }
     this.zeroBalance = function() { //only use this function when needing to overdraft. 
-
-    }
+            this.balance = 0;
+        }
+        //TODO: put the bank element as a property here. 
 }
 
 function otherAccCanOverDraft(amount) {
@@ -28,10 +30,16 @@ const checkingAcc = new Account('checking');
 const savingAcc = new Account('saving');
 const allAcc = [checkingAcc, savingAcc];
 
+
 // I think I need to make a person object and add the actual accounts to their key values pairs, upon which point I can loop 
 
 module.exports = Account;
 
 //TODO: determine if I should be exporting the instance of the object or the actual function and generate a copy there. 
 
-//FRONT END: Capable of entering amount then withdrawing/depositing it / which account
+//FRONT END: run a function that updates all the balance values when doing a deposit or withdrawal. 
+//TODO: if you use the same names for the props as in the the front end code you can save alot of repeated code.
+
+// const $checkingBalance = $('checking ')
+
+//TODO: rework the html so that you can generate new accounts by pressing buttons with automatic account id generation.
