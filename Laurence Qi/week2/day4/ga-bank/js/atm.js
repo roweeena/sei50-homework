4f5b421d80e208b7b7feea6a8fb4134aacda4a08
Laurence Qi <laurence.qi@outlook.com>
2022-01-08 14:07:55 +1100

    Cleaned Code

--- Laurence Qi/week2/day4/ga-bank/js/atm.js
+++ Laurence Qi/week2/day4/ga-bank/js/atm.js
@@ -1,48 +1,72 @@
-// DO the backend bank code first. 
-// Do some testing with it, then after get the values generated from the bank backend to bind to the visual elements.
-
-function Account(accType, balance = 0) { //TODO: initialise this as a constructor initially then convert to class. 
+// Account Constructor 
+function Account(accType, balance = 0) {
     this.accType = accType; // just a string
     this.balance = balance;
     this.deposit = function(amount) {
-        this.balance += amount;
-    }
-    this.withdraw = function(amount) { //TODO: figure out how to check other account balances. MAYBE HAVE BANK NUMBERS AND A PROPERTY IN THIS CONSTRUCTOR THAT IS ARR WITH LINKS TO OTHER ACCOUNTS
+        if (this.isNumber(amount)) {
+            this.balance += amount;
+        } else {
+            console.log("please enter a number into the diplay box");
+        }
+    };
+    this.withdraw = function(amount) {
         if (this.balance >= amount) {
             this.balance -= amount;
         } else {
-            // attempting overdraft
-            otherAccCanOverDraft(amount) ? this.balance -= balance : console.log('Can not withdraw, current balance is too low :(');
-            //TODO: remove the money from one of the other accounts and zero the balance here. 
+            canOverDraft(amount) ? this.balance -= balance : console.log('Can not withdraw, current balance is too low :(');
         }
-    }
+    };
     this.zeroBalance = function() { //only use this function when needing to overdraft. 
-            this.balance = 0;
-        }
-        //TODO: put the bank element as a property here. 
+        this.balance = 0;
+    };
+    //TODO: put the bank element as a property here. 
+    this.isNumber = function(amount) {
+        return typeof amount === 'number';
+    }
 }
 
-function otherAccCanOverDraft(amount) {
-    return amount <= allAcc[0].balance + allAcc[1].balance; //TODO: rework the sum account balances.
+function canOverDraft(amount) {
+    return amount <= allAcc[0].balance + allAcc[1].balance;
 }
+// module.exports = Account; //UNCOMMENT THIS TO TEST
 
-const checkingAcc = new Account('checking');
-const savingAcc = new Account('saving');
+const checkingAcc = new Account('checking', 1000);
+const savingAcc = new Account('savings', 500);
 const allAcc = [checkingAcc, savingAcc];
 
-
-// I think I need to make a person object and add the actual accounts to their key values pairs, upon which point I can loop 
-
-// module.exports = Account;
-
-//TODO: determine if I should be exporting the instance of the object or the actual function and generate a copy there. 
+//TODO: use the same names for the props as in the the front end to DRY your code
 
 //FRONT END: run a function that updates all the balance values when doing a deposit or withdrawal. 
-//TODO: if you use the same names for the props as in the the front end code you can save alot of repeated code.
-
-//TODO: rework the html so that you can generate new accounts by pressing buttons with automatic account id generation.
 $(function() {
     const $checkingBalance = $(`#${allAcc[0].accType} div`);
-    $checkingBalance.html('$' + allAcc[0].balance);
-    console.log($checkingBalance.html());
+    const $savingBalance = $(`#${allAcc[1].accType} div`);
+    updatBalances();
+
+    const $depositBtn = $('#checking-deposit');
+    const $withdrawBtn = $('#checking-withdraw');
+
+    //update all balances with the Account.balance value
+    function updatBalances() {
+        $checkingBalance.html('$' + checkingAcc.balance); //update balance
+        $savingBalance.html('$' + savingAcc.balance); //update balance
+
+        console.log($checkingBalance.text());
+    }
+
+    $depositBtn.on('click', function() {
+        checkingAcc.deposit(parseInt($('#checking-amount').val()));
+        updatBalances();
+    })
+
+    $withdrawBtn.on('click', function() {
+        checkingAcc.withdraw(parseInt($('#checking-amount').val()));
+        // console.log(`the current balance is ${checkingAcc.balance}`);
+        updatBalances();
+        // updatBalance()
+    }); //TODO: figure out why you can go to -$1000 just by continuously withdrawing. 
+    // make a jquery system that more easily adjusts the balances when depositing and withdrawing
 });
+
+//TODO: rework the html so that you can generate new accounts by pressing buttons with automatic account id generation.
+//TODO: maybe have a bank id whenever a new account gets created.
+//TODO: initialise this as a constructor initially then convert to class.
