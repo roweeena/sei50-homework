d9c3e57d9ddced973f72fa7fcb20160ef02fe251
Laurence Qi <laurence.qi@outlook.com>
2022-01-10 00:23:05 +1100

    refactored code and fixed the multiple instances of mapping button to Account methods

--- Laurence Qi/week2/day4/ga-bank/js/domUpdater.js
+++ Laurence Qi/week2/day4/ga-bank/js/domUpdater.js
@@ -1,19 +1,28 @@
-//FRONT END: run a function that updates all the balance values when doing a deposit or withdrawal. 
-const checkingAcc = new Account('checking', 1000);
-const savingAcc = new Account('savings', 500);
-const allAcc = [checkingAcc, savingAcc]; //TODO: you might just want to make this an object and use the name 'checking' and 'savings' as the keys to access the object. 
-let $allBtn;
+/* Using jquery update the html element values to:
+1. reflect balance 
+2. use deposit/withdraw methods from correct Account Obj instances
+*/
+
+const allAcc = [new Account('checking', 1000), new Account('savings', 500)] // Initialise the two default accounts
 
 //using jquery use the click events to trigger the withdrawal and deposit in the Account methods.
 $(function() {
-    // assign element to a prop in the Account objects.
-    allAcc.forEach(acc => { acc.$element = $(`#${acc.accType} div`) }) //TODO: figure out why this doesn't work for the last part
+    // assign each acc to a prop in the Account objects.
+    allAcc.forEach(acc => {
+        acc.$element = $(`#${acc.accType} div`)
+        checkBtn(acc.accType);
+    });
 
-    updatBalances();
-    $allBtn = $('#content [type="button"]');
+    updateBalances();
+
+    //bind the 'Add new account' button
+    $('#add-acc').on('click', function() {
+        addAccount(`additionalAcc${allAcc.length -1}`);
+        updateBalances();
+    });
 
     //updates all balance elements with the Account.balance value
-    function updatBalances() {
+    function updateBalances() {
         // update balances and background color. 
         allAcc.forEach(acc => {
             acc.$element.html('$' + acc.balance);
@@ -21,37 +30,34 @@ $(function() {
         });
     }
 
-
-    // Parses withdrawal/depositing amount and updates the account state, 
-    checkBtns();
-
-    // each time there are new elements you need to bind their input boxes and events to update the state of the respective Account Obj
-    function checkBtns() {
-        $allBtn.on('click', function() {
-            console.log('button clicked');
+    // With each new account bind the buttons to perform the deposit/withdraw methods of the respective obj
+    function checkBtn(accType) { // TODO: TALK ABOUT PROBLEM WITH THIS.
+        $(`#${accType} [type="button"]`).on('click', function() { //This used to use allBtns
             const btnType = $(this).attr('value');
             const accountName = $(this).parent().attr('id');
             const currentAcc = allAcc.find(acc => acc.accType === accountName); // find the specific acc object belonging to the div in which button was pressed
-            // const currentAcc = accountName === 'checking' ? checkingAcc : savingAcc;
             const amount = parseInt($(`#${accountName} [type="text"]`).val());
 
-            btnType === 'Deposit' ? currentAcc.deposit(amount) : currentAcc.withdraw(amount); //Withdraw/Deposit. 
+            (btnType === 'Deposit') ? currentAcc.deposit(amount): currentAcc.withdraw(amount); // perform withdraw/deposit depending on btntype 
 
-            updatBalances();
+            updateBalances();
         });
     }
 
-    $('#add-acc').on('click', function() {
-        addAccount(`additionalAcc${allAcc.length -1}`);
-        updatBalances();
-    });
-
-    function addAccount(accType, intialBalance) {
+    // push a new instance of the Account obj to the allAcc arr and add the mapped Account element to the DOM 
+    function addAccount(accType, initialBalance) {
         allAcc.push(new Account(accType, intialBalance = 0));
+        if (allAcc.length % 2 === 1) {
+            $('#content').append($('<br/>'))
+        }
 
+        createNewAccElement(accType, initialBalance);
+    }
+
+    function createNewAccElement(accType, initialBalance) {
         const $wrapper = $('<div/>', { class: 'account', id: accType });
         const $header = $(`<h1>${accType}<h1/>`);
-        const $balanceDisplay = $('<div/>', { class: 'balance', id: `${accType}-balance`, style: 'background-color: rgb(227, 227, 227)', text: `$${intialBalance}` });
+        const $balanceDisplay = $('<div/>', { class: 'balance', id: `${accType}-balance`, style: 'background-color: rgb(227, 227, 227)', text: `$${initialBalance}` });
         const $textInput = $('<input/>', { id: `${accType}-amount`, type: 'text', placeholder: 'enter an amount' })
         const $depositBtn = $('<input/>', { id: `${accType}-deposit`, type: 'button', value: 'Deposit' })
         const $withdrawBtn = $('<input/>', { id: `${accType}-withdraw`, type: 'button', value: 'Withdraw' })
@@ -62,21 +68,10 @@ $(function() {
             .append($textInput)
             .append($depositBtn)
             .append($withdrawBtn)
-
-        if (allAcc.length % 2 === 1) {
-            $wrapper.append($('<br>'))
-        }
-
-        $wrapper.appendTo($('#content'));
+            .appendTo($('#content'));
 
         allAcc[allAcc.length - 1].$element = $(`#${allAcc[allAcc.length - 1].accType} div`);
-        // console.log('the length of allAcc is:', allAcc.length);
-        // adds a new div with the relevent info to the page. 
-        $allBtn = $('#content [type="button"]');
 
-        checkBtns();
-
+        checkBtn(accType);
     }
-
-
 });
