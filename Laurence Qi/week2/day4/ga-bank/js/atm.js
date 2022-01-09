e95db1fd43ed698a42e3142d789a9a1c7c20c794
Laurence Qi <laurence.qi@outlook.com>
2022-01-09 13:39:16 +1100

    split up the atm code to have the jquery in a different file

--- Laurence Qi/week2/day4/ga-bank/js/atm.js
+++ Laurence Qi/week2/day4/ga-bank/js/atm.js
@@ -1,7 +1,8 @@
 // Account Constructor 
-function Account(accType, balance = 0) {
+function Account(accType, Initialbalance = 0) {
     this.accType = accType; // just a string
-    this.balance = balance;
+    this.balance = Initialbalance;
+
     this.deposit = function(amount) {
         if (this.isNumber(amount)) {
             this.balance += amount;
@@ -25,41 +26,6 @@ function canOverDraft(amount) {
     return amount <= allAcc[0].balance + allAcc[1].balance;
 }
 // module.exports = Account; //UNCOMMENT THIS TO TEST
-
-const checkingAcc = new Account('checking', 1000);
-const savingAcc = new Account('savings', 500);
-const allAcc = [checkingAcc, savingAcc]; // TODO: you might just want to make this an object and use the name 'checking' and 'savings' as the keys to access the object. 
-
-//FRONT END: run a function that updates all the balance values when doing a deposit or withdrawal. 
-$(function() {
-    const $checkingBalance = $(`#${allAcc[0].accType} div`);
-    const $savingBalance = $(`#${allAcc[1].accType} div`);
-    updatBalances();
-    const $allBtn = $('[type="button"]');
-
-    //updates all balance elements with the Account.balance value
-    function updatBalances() {
-        $checkingBalance.html('$' + checkingAcc.balance);
-        $savingBalance.html('$' + savingAcc.balance);
-        // console.log($checkingBalance.text());
-    }
-
-    /* The function performs the
-    1. for the specific button parse the Value of the element DONEEEE
-    2. get the value of the parent div (Checking || Savings) DONEEEE
-    3. use these values to deposit and withdraw from the specific account. DONEEEE
-    */
-    $allBtn.on('click', function() {
-        const btnType = $(this).attr('value');
-        const accountName = $(this).parent().attr('id');
-        const currentAcc = accountName === 'checking' ? checkingAcc : savingAcc;
-        const amount = parseInt($(`#${accountName} [type="text"]`).val());
-
-        btnType === 'Deposit' ? currentAcc.deposit(amount) : currentAcc.withdraw(amount); //Withdraw/Deposit. 
-
-        updatBalances();
-    });
-});
 
 //TODO: rework the html so that you can generate new accounts by pressing buttons with automatic account id generation.
 //TODO: maybe have a bank id whenever a new account gets created.
---
+++ Laurence Qi/week2/day4/ga-bank/js/jqueryUpdate.js
@@ -0,0 +1,36 @@
+//FRONT END: run a function that updates all the balance values when doing a deposit or withdrawal. 
+const checkingAcc = new Account('checking', 1000);
+const savingAcc = new Account('savings', 500);
+const allAcc = [checkingAcc, savingAcc]; // TODO: you might just want to make this an object and use the name 'checking' and 'savings' as the keys to access the object. 
+
+//using jquery use the click events to trigger the withdrawal and deposit in the Account methods.
+$(function() {
+    const $checkingBalance = $(`#${allAcc[0].accType} div`);
+    const $savingBalance = $(`#${allAcc[1].accType} div`);
+    updatBalances();
+    const $allBtn = $('[type="button"]');
+
+    //updates all balance elements with the Account.balance value
+    function updatBalances() {
+        $checkingBalance.html('$' + checkingAcc.balance);
+        $savingBalance.html('$' + savingAcc.balance);
+
+        // check if the balance is below zero and do the redbackground stuff
+    }
+
+    /* The function performs the
+    1. for the specific button parse the Value of the element DONEEEE
+    2. get the value of the parent div (Checking || Savings) DONEEEE
+    3. use these values to deposit and withdraw from the specific account. DONEEEE
+    */
+    $allBtn.on('click', function() {
+        const btnType = $(this).attr('value');
+        const accountName = $(this).parent().attr('id');
+        const currentAcc = accountName === 'checking' ? checkingAcc : savingAcc;
+        const amount = parseInt($(`#${accountName} [type="text"]`).val());
+
+        btnType === 'Deposit' ? currentAcc.deposit(amount) : currentAcc.withdraw(amount); //Withdraw/Deposit. 
+
+        updatBalances();
+    });
+});
