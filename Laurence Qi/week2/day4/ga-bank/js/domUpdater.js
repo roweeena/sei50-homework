4b1783b0438aa9a21ccbf61f70e3ed5721c21cc0
Laurence Qi <laurence.qi@outlook.com>
2022-01-09 18:07:47 +1100

    added function to add new account

--- Laurence Qi/week2/day4/ga-bank/js/domUpdater.js
+++ Laurence Qi/week2/day4/ga-bank/js/domUpdater.js
@@ -1,14 +1,12 @@
 //FRONT END: run a function that updates all the balance values when doing a deposit or withdrawal. 
 const checkingAcc = new Account('checking', 1000);
 const savingAcc = new Account('savings', 500);
-const allAcc = [checkingAcc, savingAcc]; // TODO: you might just want to make this an object and use the name 'checking' and 'savings' as the keys to access the object. 
+const allAcc = [checkingAcc, savingAcc]; //TODO: you might just want to make this an object and use the name 'checking' and 'savings' as the keys to access the object. 
 
 //using jquery use the click events to trigger the withdrawal and deposit in the Account methods.
 $(function() {
-    // assign element to a prop in the Account objects. 
-    for (let i = 0; i < allAcc.length; i++) {
-        allAcc[i].$element = $(`#${allAcc[i].accType} div`);
-    }
+    // assign element to a prop in the Account objects.
+    allAcc.forEach(acc => { acc.$element = $(`#${acc.accType} div`) })
 
     updatBalances();
     const $allBtn = $('[type="button"]');
@@ -22,14 +20,11 @@ $(function() {
         });
     }
 
-    /* The function performs the
-    1. for the specific button parse the Value of the element DONEEEE
-    2. get the value of the parent div (Checking || Savings) DONEEEE
-    3. use these values to deposit and withdraw from the specific account. DONEEEE
-    */
+    // Parses withdrawal/depositing amount and updates the account state, 
     $allBtn.on('click', function() {
         const btnType = $(this).attr('value');
         const accountName = $(this).parent().attr('id');
+        // convert this to use a loop and finding out the correct instance to allow for more than 2 accounts with this function. 
         const currentAcc = accountName === 'checking' ? checkingAcc : savingAcc;
         const amount = parseInt($(`#${accountName} [type="text"]`).val());
 
@@ -37,4 +32,11 @@ $(function() {
 
         updatBalances();
     });
+
+    function addAccount(accType, Initialbalance) {
+        allAcc.push(new Account(accType, Initialbalance));
+        // adds a new div with the relavent info to the page. 
+    }
+
+    addAccount('checking2')
 });
