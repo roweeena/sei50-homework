95d374903ea43146cf5390cfcfff3cef5b79137a
Daniel Lord-Doyle <dlorddoyle@gmail.com>
2022-01-18 09:32:07 +1100

    Finished the last one

---
+++ Daniel Lord-Doyle/week4/ruby_day_1/trip_calc.rb
@@ -0,0 +1,31 @@
+# Trip Calculator
+# Calculate a trip time and cost given inputs for
+
+# distance
+# miles per gallon
+# price per gallon
+# speed in miles per hour
+
+def trip_calc
+    print('Distance in KMs: ')
+    distance = gets.chomp
+    distance = distance.to_f
+    print('Kilometres per gallon: ')
+    kms_per_g = gets.chomp
+    kms_per_g = kms_per_g.to_f
+    print('Price per gallon: ')
+    price_per_g = gets.chomp
+    price_per_g = price_per_g.to_f
+    print('Speed in km per hour: ')
+    speed_in_kmh = gets.chomp
+    speed_in_kmh = speed_in_kmh.to_f
+
+    trip_time = "It will take #{distance / speed_in_kmh} hours."
+    puts trip_time
+
+    trip_gallons = distance / kms_per_g
+    trip_cost = "It will cost $#{trip_gallons * price_per_g} in fuel."
+    puts trip_cost
+end
+
+trip_calc
