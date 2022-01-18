f00071553dcb6381dd6840cc4eca3009c1749151
Daniel Lord-Doyle <dlorddoyle@gmail.com>
2022-01-19 08:58:08 +1100

    3 versions of MTA in Ruby. First (not v good), second (v short), third (short with commenting / optimal)

---
+++ Daniel Lord-Doyle/week4/ruby_mta/mta_3.rb
@@ -0,0 +1,37 @@
+require 'colorize'
+
+def plan_trip line1, stop1, line2, stop2
+
+    # The hash of lines
+    lines = { :n => ['Times Square', '34th', '28th', '23rd', 'Union Square', '8th'], :l => ['8th', '6th', 'Union Square', '3rd', '1st'], :six => ['Grand Central', '33rd', '28th', '23rd', 'Union Square', 'Astor Place'], }
+
+    # Gets the index of stop 1 and stop 2
+    index_of_one = lines[line1].index stop1
+    index_of_two = lines[line2].index stop2
+
+    # Gets the index of Union Square on Line 1 and Line 2
+    index_of_Union_one = lines[line1].index 'Union Square'
+    index_of_Union_two = lines[line2].index 'Union Square'
+
+    # If stop 1 declared is after Union Square (we need to know this as it is the only intersecting stop)
+    if index_of_one > index_of_Union_one then lines[line1].reverse! end
+
+    # If stop 2 declared is before Union Square
+    if index_of_two < index_of_Union_two then lines[line2].reverse! end
+
+    # This is the actual function that understands if it is a single line trip or a multi-line trip
+    if line1 == line2 
+
+        # If it is a single line trip, just outline array between stop 1 and stop 2
+        then puts "You have a simple journey. Get on the #{line1} line and your stops are: ".green p lines[line1][index_of_one, index_of_two]
+
+        # If it is a multi-line trip then go from Stop 1 to Union Square then go to line 2 and go from Union Square to Stop 2
+        else    puts "You have a 2-line journey. Get on the #{line1} line and your stops are: ".green
+                p lines[line1][lines[line1].index(stop1), lines[line1].index('Union Square')+1]
+                puts "Then get on the #{line2} line and your stops are:".green
+                p lines[line2][lines[line2].index('Union Square'), lines[line2].index(stop2)] 
+    end 
+end
+
+# plan_trip :six, 'Astor Place', :l, '8th'
+plan_trip :n, 'Times Square', :six, '33rd'
