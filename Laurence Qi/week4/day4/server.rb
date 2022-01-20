a3bb81464a8e70ebe144439d3020c918143448c2
Laurence Qi <laurence.qi@outlook.com>
2022-01-20 22:46:01 +1100

    added new rental display page

--- Laurence Qi/week4/day4/server.rb
+++ Laurence Qi/week4/day4/server.rb
@@ -3,17 +3,66 @@ require 'sinatra/reloader'
 require 'httparty'
 require 'sqlite3'
 
-get '/' do
-    # you want to do a http request to the db get the existing data and clean through it to show on here.
+
+def db_query(sql) 
+    puts '============================'
+    puts sql
+    puts '============================'
+    
+    # Perform SQL query and save into a hash
+    # a. connect to db
     db = SQLite3::Database.new 'database.db'
     db.results_as_hash = true
-
-    @rentals = db.execute "SELECT * FROM rentals"
+    
+    # b. Execute an SQL command and save result
+    rentals = db.execute sql
+    
+    # c. Close the connection
     db.close
+    rentals
+end
+
+
+
+
+# shows all the details on the rental prop. 
+
+
+# CREATE
+get 'rental/new' do
+    
+    
+    erb :new_form
+end
+
+post '/rental' do
+    sql = "INSERT INTO (street_address, suburb, cost)
+    VALUES (
+        '#{params['street_address']}', 
+        '#{params['suburb']}',
+        #{params['cost']}
+    )"
+
+    db_query sql
+    redirect '/'
+end
+
+# READ 
+get '/' do
+    # you want to do a http request to the db get the existing data and clean through it to show on here.
+    @rentals = db_query "SELECT * FROM rentals"
+
     erb :main
 end
 
 get '/rental/:id' do
-    "rental num: #{params['id']}"
-    
+    @rental = db_query("SELECT * FROM rentals WHERE id = #{params[:id]}").first
+    puts "the rental travel_time value is #{@rental['travel_time'] == nil}"
+    erb :show_rental
 end
+
+# UPDATE TODO: finish this 
+get '/rental/:id/edit' do
+    'hello this is the rental page'
+end
+# DELETE
