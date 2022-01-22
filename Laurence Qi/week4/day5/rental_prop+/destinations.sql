dd2feafb9022f9a918cb8bd532aeec38dbb14946
Laurence Qi <laurence.qi@outlook.com>
2022-01-23 00:22:41 +1100

    added people & destinations tables & relevant sql files

---
+++ Laurence Qi/week4/day5/rental_prop+/destinations.sql
@@ -0,0 +1,68 @@
+DROP TABLE IF EXISTS destinations;
+
+CREATE TABLE destinations (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    street_address TEXT, 
+    suburb TEXT,
+    visiting_frequency FLOAT, 
+    person_id INTEGER
+);
+
+--Laurence's Destinations
+INSERT INTO destinations (street_address, suburb, visiting_frequency, person_id)
+    VALUES (
+        '12 Bowler Ave',
+        'Fairfield',
+        2,
+        1
+    );
+
+INSERT INTO destinations (street_address, suburb, visiting_frequency, person_id)
+    VALUES (
+        '15 Broadway',
+        'Ultimo',
+        2,
+        1
+    );
+
+INSERT INTO destinations (street_address, suburb, visiting_frequency, person_id)
+    VALUES (
+        '129 Harrington St',
+        'The Rocks',
+        0.3,
+        1
+    );
+
+-- Eddies Destinations
+INSERT INTO destinations (street_address, suburb, visiting_frequency, person_id)
+    VALUES (
+        '85 Castlereagh St',
+        'Sydney',
+        3,
+        2
+    );
+
+INSERT INTO destinations (street_address, suburb, visiting_frequency, person_id)
+    VALUES (
+        '657 Punchbowl road',
+        'Punchbowl',
+        0.75,
+        2
+    );
+
+INSERT INTO destinations (street_address, suburb, visiting_frequency, person_id)
+    VALUES (
+        '85 Castlereagh St',
+        'Sydney',
+        3,
+        2
+    );
+
+--Patrick's Destinations
+INSERT INTO destinations (street_address, suburb, visiting_frequency, person_id)
+    VALUES (
+        'F03, University of Sydney, Eastern Ave',
+        'Camperdown',
+        3,
+        3
+    );
---
+++ Laurence Qi/week4/day5/rental_prop+/people.sql
@@ -0,0 +1,31 @@
+DROP TABLE IF EXISTS people;
+
+CREATE TABLE people (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    name TEXT, 
+    preferred_travel TEXT,
+    time_value INTEGER
+);
+
+
+INSERT INTO people (name, preferred_travel, time_value)
+    VALUES (
+        'Laurence',
+        'TRANSIT',
+        15
+    );
+
+INSERT INTO people (name, preferred_travel, time_value)
+    VALUES (
+        'Eddie',
+        'TRANSIT',
+        25
+    );
+
+
+INSERT INTO people (name, preferred_travel, time_value)
+VALUES (
+    'Eddie',
+    'TRANSIT',
+    10
+);
