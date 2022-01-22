f39c1cb1ebd4668e3e33b4d053930bdf6e70456f
Laurence Qi <laurence.qi@outlook.com>
2022-01-22 16:57:48 +1100

    added rentals data

---
+++ Laurence Qi/week4/day5/rental_prop+/rentals.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS rental;
+
+CREATE TABLE rentals (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    street_address TEXT, 
+    suburb TEXT, 
+    cost FlOAT, 
+    total_travel_time INTEGER
+);
+
+INSERT INTO rentals (street_address, suburb, cost)
+    VALUES (
+        '1/4 Beronga Street', 
+        'North Strathfield',
+        595
+    );
+
+INSERT INTO rentals ( street_address, suburb, cost )
+    VALUES (
+        '311/6 Avenue Of Oceania',
+        'Newington',
+        650
+    );
+
