7a8eeecb544419144ad7077329821be519a790c4
Jesus-fhz <jesusflores9325@gmail.com>
2022-01-22 17:14:39 +1100

    ga-crud mta-ruby-version

---
+++ Jesus Flores/week_04_ruby/ga_crud/langs.sql
@@ -0,0 +1,55 @@
+DROP TABLE IF EXISTS languages;
+
+CREATE TABLE languages(
+    id INTEGER PRIMARY KEY  AUTOINCREMENT,
+    name TEXT,
+    description TEXT,
+    created_by TEXT,
+    alive TEXT,
+    created INTEGER,
+    logo TEXT
+);
+
+INSERT INTO  languages (name,  description, created_by, alive , created, logo)
+
+VALUES(
+    'Ruby',
+    'A dynamic, open source programming language with a focus on simplicity and productivity. It has an elegant syntax that is natural to read and easy to write.',
+    'Yukihiro Matsumoto',
+   'Yes',
+    1995,
+    'https://cdn.iconscout.com/icon/free/png-256/ruby-2752084-2284901.png'
+);
+
+INSERT INTO  languages (name,  description, created_by, alive , created, logo)
+
+VALUES(
+    'Ruby',
+    'A dynamic, open source programming language with a focus on simplicity and productivity. It has an elegant syntax that is natural to read and easy to write.',
+    'Yukihiro Matsumoto',
+    'Yes',
+    1995,
+    'https://cdn.iconscout.com/icon/free/png-256/ruby-2752084-2284901.png'
+);
+
+INSERT INTO  languages (name,  description, created_by, alive , created, logo)
+
+VALUES(
+    'Ruby',
+    'A dynamic, open source programming language with a focus on simplicity and productivity. It has an elegant syntax that is natural to read and easy to write.',
+    'Yukihiro Matsumoto',
+    'Yes',
+    1995,
+    'https://cdn.iconscout.com/icon/free/png-256/ruby-2752084-2284901.png'
+);
+
+INSERT INTO  languages (name,  description, created_by, alive , created, logo)
+
+VALUES(
+    'Ruby',
+    'A dynamic, open source programming language with a focus on simplicity and productivity. It has an elegant syntax that is natural to read and easy to write.',
+    'Yukihiro Matsumoto',
+    'Yes',
+    1995,
+    'https://cdn.iconscout.com/icon/free/png-256/ruby-2752084-2284901.png'
+);
