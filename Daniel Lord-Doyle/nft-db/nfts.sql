cd7c217d08379eb881b40f5bd664a1bd4c0e7096
Daniel Lord-Doyle <dlorddoyle@gmail.com>
2022-01-24 09:33:58 +1100

    Mostly done

--- Daniel Lord-Doyle/nft-db/nfts.sql
+++ Daniel Lord-Doyle/nft-db/nfts.sql
@@ -8,87 +8,82 @@ CREATE TABLE nfts (
     for_sale BOOLEAN,
     asking_price FLOAT,
     image_route TEXT,
-    user_id INTEGER
+    previous_owner TEXT
 );
 
-INSERT INTO nfts ( project_name, last_purchase_price, owned, for_sale, image_route, user_id )
+INSERT INTO nfts ( project_name, last_purchase_price, owned, for_sale, image_route, previous_owner, asking_price )
     VALUES (
         'Immutable Apes X',
         0.07,
         true,
         false,
         'https://i.ibb.co/qpwxsdB/ape1.png',
-        1
+        'rororotheboat',
+        2.2
     );
 
-INSERT INTO nfts ( project_name, last_purchase_price, owned, for_sale, asking_price, image_route, user_id )
+INSERT INTO nfts ( project_name, last_purchase_price, owned, for_sale, asking_price, image_route, previous_owner )
     VALUES (
         'Immutable Apes X',
         0.07,
-        true,
+        false,
         true,
         0.09,
         'https://i.ibb.co/7rfFJfN/ape2.png',
-        1
+        'whichalex'
     );
 
-INSERT INTO nfts ( project_name, last_purchase_price, owned, for_sale, image_route, user_id )
+INSERT INTO nfts ( project_name, last_purchase_price, owned, for_sale, image_route, previous_owner, asking_price )
     VALUES (
         'Immutable Apes X',
         0.062,
         true,
         false,
         'https://i.ibb.co/fkPsKNZ/ape3.png',
-        1
+        'lukehammerz',
+        0.8
     );
 
-INSERT INTO nfts ( project_name, last_purchase_price, owned, for_sale, asking_price, image_route, user_id )
+INSERT INTO nfts ( project_name, last_purchase_price, owned, for_sale, asking_price, image_route, previous_owner )
     VALUES (
         'Immutable Apes X',
         0.03,
-        true,
+        false,
         true,
         0.1,
         'https://i.ibb.co/3m8MYfq/ape4.png',
-        1
+        'jiasungasong'
     );
 
-INSERT INTO nfts ( project_name, last_purchase_price, owned, for_sale, image_route, user_id )
+INSERT INTO nfts ( project_name, last_purchase_price, owned, for_sale, image_route, previous_owner, asking_price )
     VALUES (
         'Immutable Apes X',
         0.07,
         true,
         false,
         'https://i.ibb.co/wKKcXF7/ape5.png',
-        1
+        'bigyippity',
+        0.09
     );
 
-INSERT INTO nfts ( project_name, last_purchase_price, owned, for_sale, asking_price, image_route, user_id )
+INSERT INTO nfts ( project_name, last_purchase_price, owned, for_sale, asking_price, image_route, previous_owner )
     VALUES (
         'Immutable Apes X',
         0.045,
-        true,
+        false,
         true,
         1.4,
         'https://i.ibb.co/mD5mPBS/ape6.png',
-        2
+        'cassiegotc0vid'
     );
 
-INSERT INTO nfts ( project_name, last_purchase_price, owned, for_sale, asking_price, image_route, user_id )
+INSERT INTO nfts ( project_name, last_purchase_price, owned, for_sale, asking_price, image_route, previous_owner )
     VALUES (
         'Immutable Apes X',
         0.065,
-        true,
+        false,
         true,
         3.4,
         'https://i.ibb.co/j3s7Ywk/ape7.png',
-        2
+        'camth3man'
     );
-
-
-
-
-
-
-
-
--- Daniel Lord-Doyle/nft-db/server.rb
+++ Daniel Lord-Doyle/nft-db/server.rb
@@ -35,13 +35,40 @@ end
 
 # Create
 
+get '/nft/new' do
+
+    erb :list
+end #end of /nft/new 
+
+post '/nfts/:id' do
+
+Nft.create(
+    project_name: params[:project_name],
+    last_purchase_price: params[:last_purchase_price],
+    owned: params[:owned],
+    for_sale: params[:for_sale],
+    image_route: params[:image_route],
+    previous_owner: params[:previous_owner]
+)
+
+    redirect '/inventory'
+
+end
+
 # Read
 
 get '/inventory' do
 
-    @nfts = Nft.all
+    @nfts = Nft.where(owned: true)
 
     erb :inventory
+end
+
+get '/marketplace' do
+
+    @nfts = Nft.where(owned: false)
+
+    erb :marketplace
 end
 
 # Individual nft pages
@@ -54,6 +81,62 @@ get '/nfts/:id' do
 
 end
 
+# Show prev transactions for those in Inventory
+
+get '/transactions' do
+
+    @nfts = Nft.where(owned: true)
+
+    erb :transactions
+
+end
+
 # Update
 
+get '/nfts/:id/buy' do
+    
+    @nft = Nft.all.find params[:id]
+
+    erb :buy_nft
+end
+
+get '/nfts/:id/sell' do
+    
+    @nft = Nft.all.find params[:id]
+
+    erb :sell_nft
+end
+
+post '/nfts/:id/update' do
+
+    nft = Nft.find params[:id]
+
+    nft.update(
+        owned: params[:owned]
+    )
+    
+    redirect "/nfts/#{params[:id]}"
+    
+end
+
+post '/nfts/:id/sell' do
+
+    nft = Nft.find params[:id]
+
+    nft.update(
+        owned: params[:owned]
+    )
+    
+    redirect "/nfts/#{params[:id]}"
+    
+end
+
 # Delete
+
+get '/nfts/:id/delete' do
+    
+    Nft.destroy params[:id]
+
+    redirect '/inventory'
+
+end
