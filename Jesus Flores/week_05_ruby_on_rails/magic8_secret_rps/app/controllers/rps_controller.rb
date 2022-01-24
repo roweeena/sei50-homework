48dcbd45ff270eb5eb1e28c05ebbeaddee5467e7
Jesus Flores <jesus@192-168-1-105.tpgi.com.au>
2022-01-25 10:52:16 +1100

    fixing typo in controller

--- Jesus Flores/week_05_ruby_on_rails/magic8_secret_rps/app/controllers/rps_controller.rb
+++ Jesus Flores/week_05_ruby_on_rails/magic8_secret_rps/app/controllers/rps_controller.rb
@@ -7,7 +7,7 @@ class RpsController < ApplicationController
         wincondition = (@p1+@p2).to_sym
         @conditions = { rockpaper: 'paper', rockscissors: 'rock', paperscissors: 'scissors',
                         paperrock:  'paper',scissorspaper: 'scissors', scissorsrock: 'rock',
-                        paperper: 'draw', scissorsscissors: 'draw', paperpaper: 'draw' } 
+                        rockrock: 'draw', scissorsscissors: 'draw', paperpaper: 'draw' } 
         @message = @conditions[wincondition]
         
     end
