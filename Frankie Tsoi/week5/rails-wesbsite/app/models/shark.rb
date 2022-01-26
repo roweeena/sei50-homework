60e3e02aca51327a7fa614d2bb67739e551e42fb
Frankie Tsoi <email@example.com>
2022-01-26 17:22:05 +1100

    homework Rails-Shark-Website

---
+++ Frankie Tsoi/week5/rails-wesbsite/app/models/shark.rb
@@ -0,0 +1,12 @@
+class Shark < ApplicationRecord 
+
+
+  # this model file shark.rb inherit from Active Record
+
+  # this models file tells ActiveRecord where to find your database table  - it uses the pluralised lowercase version of this class name.
+
+  #Later on, we will also specify and associations here, such as 'has_many :teeths' etc
+
+  #much later on,you can also dfine your own methods here to specify business logic for this model
+
+end # of class Shark
