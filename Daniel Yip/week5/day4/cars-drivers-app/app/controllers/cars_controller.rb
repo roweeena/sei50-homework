5971f80c910d543583dfadc22355985451d138ca
danyip <95078748+danyip@users.noreply.github.com>
2022-01-28 09:36:18 +1100

    homework MVP complete....

--- Daniel Yip/week5/day4/cars-drivers-app/app/controllers/cars_controller.rb
+++ Daniel Yip/week5/day4/cars-drivers-app/app/controllers/cars_controller.rb
@@ -4,6 +4,8 @@ class CarsController < ApplicationController
   end
 
   def create
+    Car.create car_params
+    redirect_to cars_path
   end
 
   def index
@@ -19,8 +21,19 @@ class CarsController < ApplicationController
   end
 
   def update
+    car = Car.find params[:id]
+    car.update car_params
+    redirect_to car_path car
   end
 
   def destroy
+    Car.destroy params[:id]
+    redirect_to cars_path
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:make, :model, :manufacture_date, :registered, :cool_factor, :image, :driver_id)
   end
 end
