293f71000d5f93026ae9049240382c3e48d3e817
danyip <95078748+danyip@users.noreply.github.com>
2022-01-28 00:28:14 +1100

    added edit and delete to drivers

--- Daniel Yip/week5/day4/cars-drivers-app/app/controllers/drivers_controller.rb
+++ Daniel Yip/week5/day4/cars-drivers-app/app/controllers/drivers_controller.rb
@@ -4,6 +4,8 @@ class DriversController < ApplicationController
   end
 
   def create
+    Driver.create driver_params
+    redirect_to drivers_path
   end
 
   def index
@@ -19,8 +21,22 @@ class DriversController < ApplicationController
   end
 
   def update
+    driver = Driver.find params[:id]
+    driver.update driver_params
+    redirect_to driver_path params[:id]
   end
 
   def destroy
+    Driver.destroy params[:id]
   end
+
+  private
+
+  def driver_params
+    params.require(:driver).permit(:name, :dob, :licenced, :image)
+  end
+
+  
+
+
 end
