de41b7a50be76353d29700d518ae9a6d8e7708b0
Alex King <calicoalix@gmail.com>
2022-02-08 10:50:33 +1100

    unfinished :/

---
+++ Alex King/WK7HW/DAY1/movie-db/js/main.js
@@ -0,0 +1,239 @@
+
+$(() => {
+    // console.log('DOM LOADED')
+
+    function homePage(){
+        $('#output').html(`<h4>Today's top picks:`)
+
+        function popularSort(){
+            const xhr = new XMLHttpRequest();
+            xhr.onload = function(){
+                $('#output').append(`<h5>Popular:</h5><div id="topOutput"></div>`)
+                const data = JSON.parse(xhr.response);
+    
+                for (let i = 0; i<10; i++){
+                    let a = data.results[i]
+                    $('#topOutput').append(
+                        `<div class="homefilm" id="${a.id}"> 
+
+                            <div class="hometitle">
+                                <p>${a.title}</p>
+                            </div> 
+    
+                            <div class="homeposter">
+                                <img src="https://image.tmdb.org/t/p/w200/${a.poster_path}">
+                            </div>
+    
+                            <div class="homescore">
+                                <p>★${a.vote_average}</p>
+                            </div>
+    
+                        </div>`)
+                } // for loop
+                $('.homefilm').on('click', () => {
+                    $('#output').html('')
+                    console.log('hello')
+                    console.log($(this).attr("id"))
+                    // $(this).attr("id")
+                })
+            } //xhr.onload
+            xhr.open('GET', 'https://api.themoviedb.org/3/movie/popular?api_key=24d863d54c86392e6e1df55b9a328755&language=en-US&page=1')
+            xhr.send();
+
+        } //popularSort()
+
+        function topSort(){
+            const xhr = new XMLHttpRequest();
+            xhr.onload = function(){
+                $('#output').append(`<h5>Top Rated:</h5> <div id="ratedOutput"></div>`)
+                const data = JSON.parse(xhr.response);
+    
+                for (let i = 0; i<10; i++){
+                    let a = data.results[i]
+                    $('#ratedOutput').append(
+                        `<div class="homefilm"> 
+                            <div class="hometitle">
+                                <p>${a.title}</p>
+                            </div> 
+    
+                            <div class="homeposter">
+                                <img src="https://image.tmdb.org/t/p/w200/${a.poster_path}">
+                            </div>
+    
+                            <div class="homescore">
+                                <p>★${a.vote_average}</p>
+                            </div>
+    
+                        </div>`)
+                }
+                $('.homefilm').on('click', () => {
+                    $('#output').html('')
+                    console.log('hello')
+                    // $(this).attr("id")
+                })
+            }
+            xhr.open('GET', 'https://api.themoviedb.org/3/movie/top_rated?api_key=24d863d54c86392e6e1df55b9a328755&language=en-US&page=1&primary_release_date.gte=2021-12-01')
+            xhr.send();
+        } // topSort()
+
+        popularSort()
+        topSort() 
+
+        
+    } //homePage()
+
+    homePage()
+        $('.homefilm').on('click', () => {
+            $('#output').html('')
+            console.log('hello')
+            $(this).attr("id")
+        })
+
+    function loadFilm(){
+        const xhr = new XMLHttpRequest();
+        xhr.onload = function(){
+            const data = JSON.parse(xhr.response);
+            window.Response = data;
+
+            resultsLoop = data.results.forEach((a) => {
+                $('#output').append(
+                    `<div class="film"> 
+                        <div class="title">
+                            ${a.title}
+                        </div> 
+
+                        <div class="poster">
+                            <img src="https://image.tmdb.org/t/p/w200/${a.poster_path}">
+                        </div>
+
+                        <div class="release">
+                            ${a.release_date}
+                        </div>
+
+                        <div class="score">
+                            ★${a.vote_average}
+                        </div>
+
+                    </div>`)
+            });
+
+            console.log(resultsLoop)
+        }
+        const userQuery = $('#userQuery').val();
+        // console.log(userQuery)
+        xhr.open('GET', `https://api.themoviedb.org/3/search/movie?api_key=24d863d54c86392e6e1df55b9a328755&query=${userQuery}`)
+        xhr.send();
+
+        // console.log('This line runs BEFORE we get a response... too soon!')
+    } // loadFilm
+    
+    $(() => {
+        // loadFilm();
+        $('#loadMore').on('click', () => {
+            $('#output').html('')
+            loadFilm();
+        });
+
+        $('#goHome').on('click', () => {
+            $('#output').html('<div id="homeOutput"></div>') 
+            homePage();
+        });
+
+        $('#topOutput').on('click', '.homefilm', () =>{
+            $('#output').html('')
+            console.log('hello')
+            $(this).attr("id")
+        })
+    }); 
+
+
+}); //
+
+// function homePage(){
+//     return new Promise(resolve => {
+//         setTimeout( () => {
+//             $('#output').html(`<h4>Today's top picks:`)
+
+//             function popularSort(){
+//                 const xhr = new XMLHttpRequest();
+//                 xhr.onload = function(){
+//                     $('#output').append(`<h5>Popular:</h5><div id="topoutput"></div>`)
+//                     const data = JSON.parse(xhr.response);
+        
+//                     for (let i = 0; i<10; i++){
+//                         let a = data.results[i]
+//                         $('#topoutput').append(
+//                             `<div class="homefilm" id="${a.id}"> 
+//                                     <div class="hometitle">
+//                                         <p>${a.title}</p>
+//                                     </div> 
+            
+//                                     <div class="homeposter">
+//                                         <img src="https://image.tmdb.org/t/p/w200/${a.poster_path}">
+//                                     </div>
+            
+//                                     <div class="homescore">
+//                                         <p>★${a.vote_average}</p>
+//                                     </div>
+            
+//                                 </div>`)
+//                     }
+//                 } 
+//                 xhr.open('GET', 'https://api.themoviedb.org/3/movie/popular?api_key=24d863d54c86392e6e1df55b9a328755&language=en-US&page=1')
+//                 xhr.send();
+//             } //popularSort()
+    
+//             function topSort(){
+//                 const xhr = new XMLHttpRequest();
+//                 xhr.onload = function(){
+//                     $('#output').append(`<h5>Top Rated:</h5> <div id="ratedoutput"></div>`)
+//                     const data = JSON.parse(xhr.response);
+        
+//                     for (let i = 0; i<10; i++){
+//                         let a = data.results[i]
+//                         $('#ratedoutput').append(
+//                             `<div class="homefilm"> 
+//                                 <div class="hometitle">
+//                                     <p>${a.title}</p>
+//                                 </div> 
+        
+//                                 <div class="homeposter">
+//                                     <img src="https://image.tmdb.org/t/p/w200/${a.poster_path}">
+//                                 </div>
+        
+//                                 <div class="homescore">
+//                                     <p>★${a.vote_average}</p>
+//                                 </div>
+        
+//                             </div>`)
+//                     }
+//                 }
+//                 xhr.open('GET', 'https://api.themoviedb.org/3/movie/top_rated?api_key=24d863d54c86392e6e1df55b9a328755&language=en-US&page=1&primary_release_date.gte=2021-12-01')
+//                 xhr.send();
+                
+//             } // topSort()
+
+//             popularSort()
+//             topSort() 
+
+//             resolve('resolved')
+        
+//         }, 10)
+        
+//     })
+    
+// } //homePage()
+
+// // homePage()
+// async function allowClick(){
+//     console.log('waiting')
+//     const result = await homePage();
+//     console.log('done')
+//     console.log(result)
+//     $('.homefilm').on('click', () => {
+//         $('#output').html('')
+//         console.log('hello')
+//         $(this).attr("id")
+//     })
+// }
+// allowClick()
