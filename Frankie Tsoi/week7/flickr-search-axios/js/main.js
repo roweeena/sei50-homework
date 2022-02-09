0c0531dadb5f313a204988660a06d376984acae6
Frankie Tsoi <email@example.com>
2022-02-09 22:30:12 +1100

    still working

--- Frankie Tsoi/week7/flickr-search-axios/js/main.js
+++ Frankie Tsoi/week7/flickr-search-axios/js/main.js
@@ -1,8 +1,6 @@
 console.log('loaded');
 
 const FLICKR_API_KEY = '2f5ac274ecfac5a455f38745704ad084';
-const FLICKR_BASE_URL = 'https://www.flickr.com/services/rest';
-
 
 //Params:
 //method=flickr.photos.search
@@ -16,36 +14,69 @@ const FLICKR_BASE_URL = 'https://www.flickr.com/services/rest';
 
 
 $(function(){
+  
+  const fetchSearchResults = function( searchWord ){
 
-  axios.get(FLICKR_BASE_URL,{
-    params:{
-      method: 'flickr.photos.search',
-      api_key: FLICKR_API_KEY,
-      format: 'json',
-      nojsoncallback: 1,
-      text: 'coral ocean'
-    }
-  })
+    
+    const FLICKR_BASE_URL = `https://api.flickr.com/services/rest?method=flickr.photos.search&text=${searchWord}&api_key=2f5ac274ecfac5a455f38745704ad084`;
+
+    axios.get(FLICKR_BASE_URL,{
+      params:{
+        method: 'flickr.photos.search',
+        api_key: FLICKR_API_KEY,
+        format: 'json',
+        nojsoncallback: 1,
+        text: 'searchWord'
+      }
+    })
     .then(function(res){
-      console.log(res.data.photos.photo);
-      countResults()
+      // console.log(res.data.photos.total);
+      countResults(res.data.photos);
+      const showPhotos =  function(data){
+        let photos = '<ul>';
+          
+            data.forEach(function(photo){
+              console.log(photo);
+              photos +=`
+              <li data-photo-id="${photo.id}">
+          
+                ${photo.title}
+              </li>
+              `;
+            });
+            photos += '</ul>';
+          
+            $output.append(photos);
+        }
     })
     .catch(function(err){
       console.log('AJAX search error:', err);
     });
+  }
+  const countResults = function( data ){
+      console.log( 'countResults()', data );
+      const $output = $('#output');
+      
+    
+      $output.html(`<p>${data.total} matches found: </p>`)
+    };
 
-
-
-
-
+    let photos = '<ul>';
+  
+    data.forEach(function(photo){
+      console.log(photo);
+      photos +=`
+      <li data-photo-id="${photo.id}">
+  
+        ${photo.title}
+      </li>
+      `;
+    });
+    photos += '</ul>';
+  
+    $output.append(photos);
 
     
-    
-    
-    
-
-
-
     $('#userQuery').focus();
   
     $('#searchForm').on('submit', function( ev ){
@@ -56,5 +87,6 @@ $(function(){
   
       fetchSearchResults(query);
     })
-    
+  
+
 })//DOM ready
