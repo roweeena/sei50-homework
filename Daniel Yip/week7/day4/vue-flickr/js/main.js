17af28b3df946081f5c36d597e54b7cc74715e82
danyip <danielyip90@gmail.com>
2022-02-10 23:30:23 +1100

    w7 d4 basic vue flickr

--- Daniel Yip/week7/day4/vue-flickr/js/main.js
+++ Daniel Yip/week7/day4/vue-flickr/js/main.js
@@ -14,13 +14,19 @@ const vueApp = new Vue({
     searchText: "ocean coral",
     numResults: 0,
     imageUrlList: [],
-    showResults: true,
+    showResults: false,
     singleShow: false,
-    singleUrl: ''
+    loading: false,
+    singleUrl: '',
+    singleTitle: '',
+    singleDescription: '',
 
   },
   methods: {
     loadSearchResults: async function(pageNum){
+      this.showResults = false;
+      this.singleShow = false;
+      this.loading = true;
       console.log('loadSearchResults');
       try {
         const res = await axios.get(FLICKR_BASE_URL, {
@@ -34,15 +40,17 @@ const vueApp = new Vue({
             page: pageNum
           }
         })
-        console.log(res);
-        this.makeURLs(res)
+        // console.log(res);
+        this.addToImageUrlList(res)
+        this.loading = false;
+        this.showResults=true;
 
       } catch (err) {
         console.log('AJAX SEARCH ERROR: ', err);
       }
     },
 
-    makeURLs: function (res) {
+    addToImageUrlList: function (res) {
       res.data.photos.photo.forEach(photo => {
         this.imageUrlList.push({
           url: `${FLICKR_IMAGE_URL}${photo.server}/${photo.id}_${photo.secret}_q.jpg`,
@@ -52,20 +60,45 @@ const vueApp = new Vue({
       });
     },
 
-    imageClickHandler: function(id){
+    loadSingleResult: async function (id) {
+
       this.showResults = false;
-      this.singleShow = true;
-      console.log(id);
+      this.singleShow = false;
+      this.loading = true;
+
+      try {
+        const res = await axios.get(FLICKR_BASE_URL, {
+          params:{
+            method: "flickr.photos.getInfo",
+            api_key: FLICKR_API_KEY,
+            format: "json",
+            nojsoncallback: 1,
+            photo_id: id
+          }
+        })
+      
+        this.showSingleImage(res)
+        this.singleShow = true;
+        this.loading = false;
+
+      } catch (err) {
+        console.log('LOAD SINGLE AXIOS ERROR', err);
+      }
+    },
+
+    showSingleImage: function (res) {
+      const photo = res.data.photo;
+      this.singleUrl = `${FLICKR_IMAGE_URL}${photo.server}/${photo.id}_${photo.secret}_b.jpg`;
+      this.singleDescription = photo.description._content;
+      this.singleTitle = photo.title._content;
     },
 
     closeSingle: function () {
       this.showResults = true;
       this.singleShow = false;
-    }
+    },
     
   },
-
-
 
 })
 
