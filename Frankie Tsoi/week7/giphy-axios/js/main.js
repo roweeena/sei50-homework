2fea82b915822ccfac17fe9f9ec0cf4aab4eb1af
Frankie Tsoi <email@example.com>
2022-02-10 21:34:21 +1100

    got the image showing YES!!!!!

--- Frankie Tsoi/week7/giphy-axios/js/main.js
+++ Frankie Tsoi/week7/giphy-axios/js/main.js
@@ -3,61 +3,62 @@ console.log('Loaded!');
 const giphyAPI = 'jti3xBGEdwwauf1muQ3bIJEDBjZJJr60'
 const giphyURL = 'https://api.giphy.com/v1/gifs/search'
 
-const fetchSearchResults = function( searchWord ){
-  console.log('fetchSearchResults(): ', searchWord);
+// const url = `https://api.giphy.com/v1/gifs/search?api_key=jti3xBGEdwwauf1muQ3bIJEDBjZJJr60&q=${searchWord}&limit=25&offset=0&rating=g&lang=en`;
 
 
+$(function () {
 
-  // const url = `https://api.giphy.com/v1/gifs/search?api_key=jti3xBGEdwwauf1muQ3bIJEDBjZJJr60&q=${searchWord}&limit=25&offset=0&rating=g&lang=en`;
+  const fetchSearchResults = function (searchWord) {
+    console.log('fetchSearchResults(): ', searchWord);
 
-  axios.get(giphyURL,{
-    params:{
-      api_key: giphyAPI,
-      q: searchWord,
-      limit: 25,
-    }
-  })
-  .then(function(res){
-    console.log('Success!', res.data);
-    renderSearchResults(res.data);
-  })
-  .catch(function(err){
-    console.log('Problem!', err);
-  });
+    axios.get(giphyURL, {
+      params: {
+        api_key: giphyAPI,
+        q: searchWord,
+        limit: 25,
+      }
+    })
+      .then(function (res) {
+        console.log('Success!', res.data);
+        renderSearchResults(res.data);
+      })
+      .catch(function (err) {
+        console.log('Problem!', err);
+      });
 
-}; // fetchSearchResults
+  }; // fetchSearchResults
 
-const renderSearchResults = function( data ){
-  console.log('renderSearchResults()', data);
-  const $output = $('#output');
-  // $output.show(); // incase some submits a search from a show page ie results are hidden
-  // $('#details').hide();
-  
-  $output.html(`<p>${data.pagination.total_count} matches found: </p>`)
-  
-  let giphys = '<ul>';
-  data.data.forEach(function(giphy){
-    // console.log(giphy);
-    giphys +=`
+  const renderSearchResults = function (data) {
+    console.log('renderSearchResults()', data);
+    const $output = $('#output');
+    // $output.show(); // incase some submits a search from a show page ie results are hidden
+    // $('#details').hide();
+    $output.html(`<p>${data.pagination.total_count} matches found: </p>`)
+
+   
+    let giphys = '<ul>';
+    data.data.forEach(function (giphy) {
+      console.log(giphy);
+      giphys += `
     <li data-giphy-id="${giphy.id}">
     ${giphy.title}
-    <img src="${giphy.images.url}">
+    
+    <img src="${giphy.images.original.url}">
     </li>
     `;
-  });
-  giphys += '</ul>';
+    });
+    giphys += '</ul>';
 
-  $output.append(giphys);
+    $output.append(giphys);
+
+  };
 
 
-};
 
-
-$(function(){
-
+  // the below is the submit search button
   $('#userQuery').focus();
 
-  $('#searchForm').on('submit', function( ev ){
+  $('#searchForm').on('submit', function (ev) {
     ev.preventDefault();
     console.log('Form submitted!');
 
