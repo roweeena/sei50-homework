e638359ad5ace81b818b820480a6fdac06af5da3
Steven Furnari <s.furnari@hotmail.com>
2022-02-11 01:05:25 +1100

    infinite scrolling added

--- Steven Furnari/flickr-spa_0902/js/main.js
+++ Steven Furnari/flickr-spa_0902/js/main.js
@@ -1,94 +1,105 @@
-$(function(){
+
+const API_KEY = '2f5ac274ecfac5a455f38745704ad084';
+
+const FLICKR_URL = 'https://api.flickr.com/services/rest';
+
+let page = 1
+
+const getSearch = function() {
+    const userQuery = $('#userQuery').val();
     
-    const API_KEY = '2f5ac274ecfac5a455f38745704ad084';
+    axios.get(FLICKR_URL, {params: {
+        method: 'flickr.photos.search',
+        api_key: API_KEY,
+        format: 'json',
+        nojsoncallback: 1,
+        safety_level: 1,
+        text: userQuery,
+        page: page
+    }}) // get()
+    .then(res => {
+        renderSearch(res.data.photos.photo)
+    }) // then()
+    .catch(err => {
+        console.log('AJAX Search Error: ', err);
+    }) // catch()
+}; // getSearch()
 
-    const FLICKR_URL = 'https://api.flickr.com/services/rest'
+const renderSearch = function(arr){
 
+    arr.forEach( (img) => {
+        const photoUrl = `https://live.staticflickr.com/${img.server}/${img.id}_${img.secret}_q.jpg`
+        $('#list').append(`<img class="list-image" data-photo="${img.id}" src="${photoUrl}"></img>`)     
+    })
+    
+    $('.list-image').on('click', function(){
+        $(window).off('scroll')
+        const photoID = $(this).data('photo')
+        getShow(photoID)
+        $('#list').hide()
 
-// Params:
-// method, api_key - REQUIRED
-// format ??? for specifying format
-// text - specify search query
+    }) // on click for each photo
+    
+}; // renderSearch()
 
+const getShow = function(id){
 
-    const getSearch = function(){
-        const userQuery = $('#userQuery').val();
-        
-        axios.get(FLICKR_URL, {params: {
-            method: 'flickr.photos.search',
-            api_key: API_KEY,
-            format: 'json',
-            nojsoncallback: 1,
-            safety_level: 1,
-            text: userQuery
-        }}) // get()
-        .then(res => {
-            renderSearch(res.data.photos.photo)
-        }) // then()
-        .catch(err => {
-            console.log('AJAX Search Error: ', err);
-        }) // catch()
-    }; // getSearch()
+    axios.get(FLICKR_URL, {params: {
+        method: 'flickr.photos.getInfo',
+        api_key: API_KEY,
+        photo_id: id,
+        format: 'json',
+        nojsoncallback: 1   
+    }}) // get()
+    .then(function(res){
+        renderShow(res.data.photo)
+    }) // then()
+    .catch(function(err){
+        console.log('AJAX Show Error: ', err);
+    }) // catch()
+}; // getShow()
 
-    const renderSearch = function(arr){
- 
-        arr.forEach( (img) => {
-            const photoUrl = `https://live.staticflickr.com/${img.server}/${img.id}_${img.secret}_q.jpg`
-            $('#list').append(`<img class="list-image" data-photo="${img.id}" src="${photoUrl}"></img>`)     
-        })
-        
-        $('.list-image').on('click', function(){
-            const photoID = $(this).data('photo')
-            getShow(photoID)
-            $('#list').hide()
-        })
-    }; // renderSearch()
+const renderShow = function(photoData){
+    const imageUrl = `https://live.staticflickr.com/${photoData.server}/${photoData.id}_${photoData.secret}_c.jpg`
+    $(window).off('scroll')
+    // console.log(photoData)
 
-    const getShow = function(id){
+    $('#list').hide();
+    $('#show').show();
 
-        axios.get(FLICKR_URL, {params: {
-            method: 'flickr.photos.getInfo',
-            api_key: API_KEY,
-            photo_id: id,
-            format: 'json',
-            nojsoncallback: 1   
-        }}) // get()
-        .then(function(res){
-            renderShow(res.data.photo)
-        }) // then()
-        .catch(function(err){
-            console.log('AJAX Show Error: ', err);
-        }) // catch()
-    }
+    $('#show').append(`
+        <p><strong>Title:</strong> ${photoData.title._content}</p>
+        <p><strong>By:</strong> ${photoData.owner.realname} (${photoData.owner.username})</p>
+        <p>Veiws: ${photoData.views}</p>
+        <img src="${imageUrl}" id="show-image">
+        <p>${photoData.description._content}</p>
+        <button id="backButton">Back to Results</button>
+    `)
 
-    const renderShow = function(photoData){
-        const imageUrl = `https://live.staticflickr.com/${photoData.server}/${photoData.id}_${photoData.secret}_c.jpg`
+    $('#backButton').on('click', function(){
+        $('#show').hide();
+        $('#list').show();
+        $('#show').empty();
+        scrollListener()
+    }); // back button click handler
+    
+}; //renderShow()
 
-        $('#list').hide();
-        $('#show').show();
+$('#search').on('click', function(e){
+    $('#list').empty()
+    getSearch()
+    scrollListener()    
+    
+    e.preventDefault()
+});  // search on click
 
-        $('#show').append(`
-            <p><strong>Title:</strong> ${photoData.title._content}</p>
-            <p><strong>By:</strong> ${photoData.owner.realname} (${photoData.owner.username})</p>
-            <img src="${imageUrl}" id="show-image">
-            <p>Veiws: ${photoData.views}</p>
-            <button id="backButton">Back to Results</button>
-            `)
+const scrollListener = function(){
+    $(window).on('scroll', () => {
 
-        $('#backButton').on('click', function(){
-            $('#show').hide();
-            $('#list').show();
-            $('#show').empty()
-            }); // back button click handler
-   } //renderShow()
-
-
-
-    $('#search').on('click', function(e){
-        $('#list').empty()
-        getSearch()    
-        
-        e.preventDefault()
-    })  // search on click
-
-}) // window.load
+        if (($(document).height() * 0.9) - $(this).height() <= $(this).scrollTop()){
+            $(window).off('scroll')
+            page++
+            getSearch()
+        }
+    })
+}
