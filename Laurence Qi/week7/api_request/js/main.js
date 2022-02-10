c3ddf90caeba3a66ed312f89bbb44e353e498873
Laurence Qi <laurence.qi@outlook.com>
2022-02-11 00:08:33 +1100

    converted everything to vue

--- Laurence Qi/week7/api_request/js/main.js
+++ Laurence Qi/week7/api_request/js/main.js
@@ -1,19 +1,87 @@
-// Try to create a search page and a show (details) page, using at least two API requests
 
-// using the geo coords function, we can get the coordinates of locations
 
-// TODO: iterate through the lat long points to get the values of the locations. 
-console.log('Nasa query form loaded')
-
-//TODO: refactor all of this code to have clean layout with separated concerns
 //TODO: implement vue for the forms etc. 
 //TODO: convert geocode search to a proper submittable form that chains off of the search function
 //TODO: add loading div
 
+
+//TODO: use a Vue component for the iframe.
+// Vue.component('')
+
+
+myApp = new Vue({
+    el: '#app',
+
+    data: {
+        address: 'hello',
+        lat: 0,
+        lng: 0,
+
+        title: '',
+        nasa_img: '',
+        googleURL: ''
+    },
+    
+    methods: {
+        generateGeoCodes: async function (e){
+            e.preventDefault();
+
+            const URL_ADDRESS = this.address.replaceAll(' ', '+');
+            const GOOGLE_KEY = 'AIzaSyAm7vYw4jkC7m9hbEKpMfFxjwLAOZgxwko'
+
+            response = await axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
+                params: {
+                    address: URL_ADDRESS,
+                    key: GOOGLE_KEY
+                }
+            })
+            
+            try {
+                const coords = response.data.results[0].geometry.location // 12 bowler ave fairfield nsw australia
+                console.log(coords);
+                this.lat = coords.lat
+                this.lng = coords.lng
+            } catch (error) {
+                console.log('AJAX ERROR:', error);
+            }
+        },
+
+        searchMaps: async function (e){
+            e.preventDefault();
+
+            const date = '2018-01-01'// NOTE: this is an arbitrary date, I found that contrary to the docs, any date will not work. NASA API will not find images beyond 5 years from the query date. 
+            // const dim = $('#dim').val(); //width and height of image in degrees //FIXME: didn't find how to search for this. 
+            const NASA_KEY = 'Y3DDqVZTa23oDDvIuzdp8HotSVQdXVXZ5WmfZ5fs';
+            const NASA_BASE_URL = 'https://api.nasa.gov/planetary/earth/assets'
+        
+            const response = await axios.get( NASA_BASE_URL, {
+                params: {
+                    lat: this.lat, 
+                    lng: this.lng, 
+                    date: date, 
+                    api_key: NASA_KEY, 
+                }
+            })
+            try {
+                this.title = $(`Photo taken on ${response.data.date}`);
+                this.nasa_img = response.data.url;
+                this.googleURL = `https://www.google.com/maps/embed/v1/view?key=AIzaSyAm7vYw4jkC7m9hbEKpMfFxjwLAOZgxwko&center=${this.lat},${this.lng}&maptype=satellite`
+                $('#error').text('')
+                $('#output').empty().append(title).append(map_iframe).append(img);
+            } catch (error) {
+                console.log(error);   
+            }
+        }
+    }
+});
+
+
+
+
 $(function() {
     $('#lat').focus();
 
-    // add searching NASA satellite images by their Latitude and Longitude values. 
+    // add searching NASA satellite images by their Latitude and lnggitude values. 
     $('#search').on('submit', function(ev) {
         ev.preventDefault();
         searchMaps();
@@ -22,7 +90,7 @@ $(function() {
     // Convert address to geocodes and populate the above searchMaps fields
     $('#geocode').on('click', function(ev){
         const address = $('#address').val();
-        convertAddress2GeoCode(address);
+        generateGeoCodes(address);
     })
     // axios.get(`https://api.nasa.gov/planetary/earth/imagery?lon=${long}.75&lat=${lat}&date=${date}&api_key=${nasa_api_key}`);
 });
@@ -55,27 +123,3 @@ async function searchMaps() {
     }
 }
 
-async function convertAddress2GeoCode(address) {
-    const urlAddress = address.replaceAll(' ', '+')
-    const GOOGLE_KEY = 'AIzaSyAm7vYw4jkC7m9hbEKpMfFxjwLAOZgxwko'
-    //TODO: try out both the above url and the proper string
-
-    response = await axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
-        params: {
-            address: urlAddress,
-            key: GOOGLE_KEY
-        }
-    })
-
-    try {
-        const coords = response.data.results[0].geometry.location // 12 bowler ave fairfield nsw australia
-        console.log(coords);
-        $('#lat').val(coords.lat);
-        $('#lon').val(coords.lng);
-    } catch (error) {
-        console.log('AJAX ERROR:', error);
-    }
-
-
-    //TODO: look into the geocoded gem at the model level (RUBY).  USING THE GEO LOCATION API FROM THE BROWSER 
-}
