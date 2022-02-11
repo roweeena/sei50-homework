9fa892deadc8f9c738229200773cb69c93e6cdd1
Frankie Tsoi <email@example.com>
2022-02-12 00:01:38 +1100

    added 3 component

--- Frankie Tsoi/week7/tic-tac-toe-react/src/Square.js
+++ Frankie Tsoi/week7/tic-tac-toe-react/src/Square.js
@@ -13,16 +13,55 @@ class Square extends React.Component{
 
 }// class Square
 
+class Board extends React.Component{
+  renderSquare(i){
+    return <Square/>;
+  }// render
 
+  render() {
+    const status = 'Next Player: X';
+    return(
+      <div>
+        <div className="status">{status}</div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );//return
+  }//render
 
+}// class Board
 
+class Game extends React.Component{
+  render() {
+    return(
+      <div className="game">
+        <div className="game-board">
+          <Board/>
+        </div>
+        <div className="game-info">
+          <div>{/*status*/}</div>
+          <ol>{/*TODO*/}</ol>
+        </div>
+      </div>
+    );// return
+  }//render
+}// class Game
 
-
-
-
-
-
-
-
-
-export default Square;
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+)
