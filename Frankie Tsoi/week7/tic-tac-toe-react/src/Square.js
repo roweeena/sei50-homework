da4c1b569b7180289238fa584b8c39b65c4ffc2f
Frankie Tsoi <email@example.com>
2022-02-11 23:41:19 +1100

    created Square Components

--- Frankie Tsoi/week7/tic-tac-toe-react/src/Square.js
+++ Frankie Tsoi/week7/tic-tac-toe-react/src/Square.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css'; 
 
 
+class Square extends React.Component{
 
+  render(){
+    return(
+      <button className="square">{/* TODO */}</button>
+    ); // return
+  } // render
+
+}// class Square
 
 
 
--- Frankie Tsoi/week7/tic-tac-toe-react/src/index.js
+++ Frankie Tsoi/week7/tic-tac-toe-react/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Square from './Square';
 
 
 ReactDOM.render(
   
-    <App />,
+    <Square />,
  
   document.getElementById('root')
 );
