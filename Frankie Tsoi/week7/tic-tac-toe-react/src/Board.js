0266ea56de480b038b714859a5a26cc263e2f54f
Frankie Tsoi <email@example.com>
2022-02-12 15:56:20 +1100

    history added

--- Frankie Tsoi/week7/tic-tac-toe-react/src/Board.js
+++ Frankie Tsoi/week7/tic-tac-toe-react/src/Board.js
@@ -3,47 +3,19 @@ import React from 'react';
 import './index.css'; 
 import Square from './Square';
 
-class Board extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
+class Board extends React.Component {
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
   }
 
-  handleClick(i){
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]){
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }// handleClick
-
-  renderSquare(i){
-    return (
-    <Square value={this.state.squares[i]} 
-    onClick={() => this.handleClick(i)}/>
-    );
-  }// render
-
   render() {
-    const winner = calculateWinner(this.state.squares);
-
-    let status;
-    if (winner){
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    } // end of if and else
-
-    return(
+    return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -60,31 +32,13 @@ class Board extends React.Component{
           {this.renderSquare(8)}
         </div>
       </div>
-    );//return
-  }//render
-
-}// class Board
-
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
+    );
   }
-  return null;
 }
+
+
+
+
 
 
 export default Board;
