3af93817aff1bf5186b14bd3081f056cb5e5e799
Frankie Tsoi <email@example.com>
2022-02-12 15:13:13 +1100

    functional tic tac game

--- Frankie Tsoi/week7/tic-tac-toe-react/src/Board.js
+++ Frankie Tsoi/week7/tic-tac-toe-react/src/Board.js
@@ -14,12 +14,15 @@ class Board extends React.Component{
 
   handleClick(i){
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]){
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
       xIsNext: !this.state.xIsNext,
     });
-  }
+  }// handleClick
 
   renderSquare(i){
     return (
@@ -29,8 +32,14 @@ class Board extends React.Component{
   }// render
 
   render() {
+    const winner = calculateWinner(this.state.squares);
 
-    const status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
+    let status;
+    if (winner){
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    } // end of if and else
 
     return(
       <div>
@@ -55,6 +64,27 @@ class Board extends React.Component{
   }//render
 
 }// class Board
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
 
 export default Board;
