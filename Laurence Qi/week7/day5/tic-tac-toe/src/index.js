7d286aed008eb5966731ba8a7688bcc637be4b4b
Laurence Qi <laurence.qi@outlook.com>
2022-02-12 21:23:47 +1100

    starter DOM traversal code

--- Laurence Qi/week7/day5/tic-tac-toe/src/index.js
+++ Laurence Qi/week7/day5/tic-tac-toe/src/index.js
@@ -14,14 +14,23 @@ class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            squares: Array(9).fill(null)
+            squares: Array(9).fill(null),
+            xIsNext: true
         };
     }
     
     handleClick(i) {
         const squares = this.state.squares.slice(); // make copy of the current board state. 
-        squares[i] = 'X';
-        this.setState({squares: squares});
+
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext
+        });
     }
     
     renderSquare(i) {
@@ -34,7 +43,13 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+          status = 'Winner: ' + winner;
+        } else {
+          status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return ( //TODO: we're changing the number values here (props*)
         <div>
@@ -74,6 +89,27 @@ render() {
     );
 }
 }
+
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+}
+
 
 // ========================================
 
