bd34865ee4133a94ceecf2692a68c89f7020fc49
Frankie Tsoi <email@example.com>
2022-02-12 14:34:52 +1100

    X and O can take turns

--- Frankie Tsoi/week7/tic-tac-toe-react/src/Square.js
+++ Frankie Tsoi/week7/tic-tac-toe-react/src/Square.js
@@ -2,24 +2,13 @@ import React from 'react';
 import './index.css'; 
 
 
-class Square extends React.Component{
-
-  render(){
-    return(
-      <button className="square" 
-      onClick={() => this.props.onClick()}> 
-  
-      {this.props.value}
-      </button>
-    ); // return
-  } // render
-
-}// class Square
-
-
-
-
-
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
 
 
 
