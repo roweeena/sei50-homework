386afc7701c6407215d692168140e8c725e8ba9e
danyip <danielyip90@gmail.com>
2022-02-12 20:08:25 +1100

    search results API call working

--- Daniel Yip/week7/day5/react-flickr/src/Flickr.js
+++ Daniel Yip/week7/day5/react-flickr/src/Flickr.js
@@ -6,29 +6,19 @@ import Results from "./Results"
 class Flickr extends React.Component{
 
   state = {
-    searchValue: 'ocean coral'
+    searchValue: 'ocean coral',
+    showResults: false
   }
 
   handleChange = (e)=> {
-    console.log('hello from Flickr value = ', e.target.value );
+    // console.log('hello from Flickr value = ', e.target.value );
     this.setState({searchValue: e.target.value})
   }
 
   handleSubmit = (e) => {
     e.preventDefault()
     console.log('SUBMIT!');
-  }
-
-  fetchSearchResults = async (query) => {
-    
-    try {
-      const res = await axios.get(query)
-        
-      };
-
-    } catch (err) {
-      
-    }
+    this.setState({showResults: true})
   }
 
   render(){
@@ -36,7 +26,13 @@ class Flickr extends React.Component{
     return(
       <div className="main">
       <Header searchValue={this.state.searchValue} handleChange={()=>this.handleChange} handleSubmit={()=> this.handleSubmit}/>
-      <Results/>
+      
+      {
+        this.state.showResults ? <Results/> : <p>nothing to show</p>
+      }
+
+
+      
       </div>
     )
   }
--- Daniel Yip/week7/day5/react-flickr/src/Results.js
+++ Daniel Yip/week7/day5/react-flickr/src/Results.js
@@ -1,14 +1,49 @@
 import React from "react"
 import "./Results.css"
 import Thumbnail from "./Thumbnail"
+import axios from "axios"
 
+const FLICKR_API_KEY = "2f5ac274ecfac5a455f38745704ad084";
+const FLICKR_BASE_URL = "https://api.flickr.com/services/rest";
+const FLICKR_IMAGE_URL = "https://live.staticflickr.com/";
 
 class Results extends React.Component{
 
+  state = {
+    results: []
+  }
+
+  getSearchRes = async () => {
+    try {
+      const res = await axios.get(FLICKR_BASE_URL, {
+        params: {
+          method: "flickr.photos.search",
+          api_key: FLICKR_API_KEY,
+          format: "json",
+          nojsoncallback: 1,
+          safe_search: 1,
+          text: "ocean coral"
+        }
+      })
+      console.log(res.data.photos.photo);
+      this.setState({results: res.data.photos.photo})
+
+    } catch (err) {
+      console.log("AXIOS SEARCH ERROR", err);
+    }
+  }
+  componentDidMount(){
+
+    this.getSearchRes()
   
+  }
+
+
   render(){
     return(
       <div id="results">
+        {this.state.results.map(result => <Thumbnail key={result.id} url= {`${FLICKR_IMAGE_URL}${result.server}/${result.id}_${result.secret}_q.jpg`}/> )}
+
         <Thumbnail url="http://www.fillmurray.com/200/200"/>
       </div>
     )
