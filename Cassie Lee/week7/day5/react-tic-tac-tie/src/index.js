fac2bef37e5a32943ce52e0118b782cb32597d63
“Cassie <cassielee8757@gmail.com>
2022-02-13 22:21:01 +1100

    React Tic-Tac-Toe

---
+++ Cassie Lee/week7/day5/react-tic-tac-tie/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import TicTacToe from './TicTacToe';
+
+
+
+
+
+ReactDOM.render(
+    <TicTacToe />,
+  document.getElementById('root')
+);
+
+
