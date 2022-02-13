440baf4f7bccbb9c042b70856101c6a4e0546c32
Jesus-fhz <jesusflores9325@gmail.com>
2022-02-13 20:14:50 +1100

    Practicing react, states, components, hooks

---
+++ Jesus Flores/week7_js/tic-tac-toe-version-2/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react'
+import Board from './components/Board'
+
+function App() {
+  return (
+    <div className="App">
+        <h1>  Tic Tac Toe </h1>
+        <Board />
+    </div>
+  );
+}
+
+export default App;
