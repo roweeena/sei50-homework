d95ddde6fd3a647d2d31fee72414729e373a861e
Jesus-fhz <jesusflores9325@gmail.com>
2022-02-28 16:58:20 +1100

    adding routes

--- Jesus Flores/week_10/ba-vue-front-end/src/App.vue
+++ Jesus Flores/week_10/ba-vue-front-end/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <h1>Burning Airlines</h1>
     <router-view/>
   </div>
 </template>
---
+++ Jesus Flores/week_10/ba-vue-front-end/src/components/FlightSearch.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>Search Flights</h2>
+    </div>
+</template>
+
+<script>
+
+export default{
+  name: 'FlightSearch'
+}
+</script>
+
+<style scoped>
+
+</style>
