bd2dbe03e39a2991fcbfc1f24640622932099555
Jesus-fhz <jesusflores9325@gmail.com>
2022-02-28 15:44:55 +1100

    adding more rotues and search method

--- Jesus Flores/week_10/ba-rails-backend/app/controllers/flights_controller.rb
+++ Jesus Flores/week_10/ba-rails-backend/app/controllers/flights_controller.rb
@@ -4,5 +4,28 @@ class FlightsController < ApplicationController
         render json: Flight.all
     end
 
+    def search
+        flights = Flight.where origin: params[:origin], destination: params[:destination]
+        render json: flights, include:{
+            airplane:{
+                only: [:name]
+            }
+        }
+    end
+
+    def show
+        flight = Flight.find(params[:id])
+        render json: flight, include: {
+            airplane: {},
+            reservations: {
+                only: [:row , :col],
+                include: {
+                    user: {
+                         only: [:name]
+                    }
+                }
+            },
+        }
+    end
 
 end
--- Jesus Flores/week_10/ba-rails-backend/config/routes.rb
+++ Jesus Flores/week_10/ba-rails-backend/config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #API END-POINTS
   get '/flights' => 'flights#index'
+  get '/flights/:id' => 'flights#show'
+
+  get'/flights/search/:origin/:destination' => 'flights#search'
 end
