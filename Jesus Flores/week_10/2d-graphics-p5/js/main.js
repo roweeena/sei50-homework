a3b0fac540056561640ce25748b33606d887fdfe
Jesus-fhz <jesusflores9325@gmail.com>
2022-03-02 10:08:20 +1100

    drawing with p5

---
+++ Jesus Flores/week_10/2d-graphics-p5/js/main.js
@@ -0,0 +1,15 @@
+function setup(){
+    createCanvas(windowWidth, windowHeight);
+    
+    // strokeWeight(10);
+    // stroke(0, 0, 255);
+    // noStroke();
+    //      x    y   w   h
+    fill(255, 0, 0);
+    ellipse(50, 50, 80, 80);
+
+    fill(255, 255, 0);
+    noFill();
+    stroke(255,255, 0);
+    ellipse(windowWidth/2, windowHeight/2, 150, 150);
+}
