acd552add7a2d99fbed67a9de34ced27fe0cfcee
Jesus-fhz <jesusflores9325@gmail.com>
2022-03-01 12:41:54 +1100

    Testing negative as well

--- Jesus Flores/week_10/ba-vue-front-end/test/unit/specs/utils.spec.js
+++ Jesus Flores/week_10/ba-vue-front-end/test/unit/specs/utils.spec.js
@@ -1,11 +1,18 @@
 import {adder, subber} from '@/lib/utils'
 
-describe('Utils library', () => {
+describe('adder()', () => {
   it('should add two numberos correctly', () => {
     const result = adder(5, 8)
     expect(result).to.equal(13)
   })
 
+  it('Should also work with negative numbers', () => {
+    const result = adder(5, -10)
+    expect(typeof result).to.equal('number')
+  })
+})
+
+describe('subber()', () => {
   it('should substract two numbers correctly', () => {
     const result = subber(15, 5)
     expect(result).to.equal(10)
