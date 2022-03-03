c3e734f8d4a89e9280239e339a6643e73242a7a3
Jesus-fhz <jesusflores9325@gmail.com>
2022-03-03 16:16:47 +1100

    Fixing router errors

--- Jesus Flores/week_10/ba-vue-front-end/test/unit/specs/FlightSearchResults.spec.js
+++ Jesus Flores/week_10/ba-vue-front-end/test/unit/specs/FlightSearchResults.spec.js
@@ -1,3 +1,4 @@
+import Vue from 'vue'
 import { mount } from '@vue/test-utils'
 import sinon from 'sinon' // mock library
 
@@ -8,7 +9,7 @@ import axios from 'axios' // so we can mock it
 const fakeFlights = [
   {
     id: 1,
-    flight_number: 'BA123',
+    flight_number: 'BA256',
     departure_date: '2022-03-01T04:20:00.000Z',
     origin: 'SYD',
     destination: 'MEL',
@@ -34,6 +35,8 @@ const fakeFlights = [
   } // second flight
 ]
 
+axios.get = sinon.stub().returns(Promise.resolve({data: fakeFlights}))
+
 describe('<FlightSearchResults>', () => {
   let wrapper
   beforeEach(() => {
@@ -48,8 +51,6 @@ describe('<FlightSearchResults>', () => {
     // stub - a spy which ALSO lets you fake
     //        the behaviour (i.e. the return value)
     //        of the function you are faking
-
-    axios.get = sinon.stub().returns(Promise.resolve({data: fakeFlights}))
   }) // beforeEach
 
   it('renders without errors', () => {
@@ -57,7 +58,7 @@ describe('<FlightSearchResults>', () => {
   }) // it renders without smoke pouring out of it
 
   it('renders a list of matching flights', async () => {
-    console.log('page:', wrapper.text())
+    // console.log('page:', wrapper.text() );
 
     // The axios.get() inside the component's mounted() method
     // returns a promise, and the component will not have access to the
@@ -65,6 +66,26 @@ describe('<FlightSearchResults>', () => {
     // that axios promise resolves
     await wrapper.vm.$nextTick()
 
-    console.log('page after waiting:', wrapper.text())
+    // console.log('page after waiting:', wrapper.text());
+
+    // Test that our stubbed version (which is also a spy) was called with
+    // the correct Rails API endpoint URL (including the test origin & destination)
+    expect(axios.get).to.have.been.calledWith('http://localhost:3000/flights/search/SYD/MEL')
+
+    // It should stop showing the loading message once the response data arrives!
+    expect(wrapper.text()).to.not.contain('Loading flight results')
+
+    const results = wrapper.findAll('div.results')
+
+    // It should show the same number of results as in our fakeData flights array
+    expect(results.length).to.equal(2)
+
+    // The first flight result should contain the flight number
+    expect(results.at(0).text()).to.contain('BA256')
+
+    // The first flight result should also contain the airplane model
+    expect(results.at(0).text()).to.contain('747')
+
+    // TODO: do the same checks for the second test flight
   }) // it renders flights
 }) // describe <FlightSearchResults>
