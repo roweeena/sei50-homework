b3e0ea9531e4e77773cf0ef28ec61418e868f683
Jesus-fhz <jesusflores9325@gmail.com>
2022-03-03 17:40:40 +1100

    using child and parents components and passing props, emiting events

---
+++ Jesus Flores/week_10/ba-vue-front-end/src/components/ReservationConfirm.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <h2>Reservation Confirm</h2>
+      <h3>Your Seats: {{ letter }}{{ rows}}</h3>
+      <div class="seating">
+          <button v-on:click="confirmSeat">Confirm Reservation</button>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['cols', 'rows', 'letter'],
+  name: 'ReservationConfim',
+  methods: {
+    confirmSeat () {
+      console.log('seat confimed')
+      this.$emit('confirmSeat')// We emit event 'confirmSeat' and we listen for it in the parent.
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
