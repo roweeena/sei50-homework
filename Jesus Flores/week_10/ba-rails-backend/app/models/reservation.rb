de57d9b8ce8fe54f36989e8b78ee6686017b7cca
Jesus-fhz <jesusflores9325@gmail.com>
2022-03-04 14:58:11 +1100

    ba validations

--- Jesus Flores/week_10/ba-rails-backend/app/models/reservation.rb
+++ Jesus Flores/week_10/ba-rails-backend/app/models/reservation.rb
@@ -1,6 +1,6 @@
 class Reservation < ApplicationRecord
     belongs_to :user
     belongs_to :flight
-    
+    #Avoiding duplicate bookings
     validates :row, uniqueness: {scope: [:col, :flight_id]}
 en
