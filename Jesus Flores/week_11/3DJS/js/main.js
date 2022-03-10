e814a4612a6985c5b470184f3ba0eb2847d73227
Jesus-fhz <jesusflores9325@gmail.com>
2022-03-10 11:18:43 +1100

    adding scene to canvas and axis

--- Jesus Flores/week_11/3DJS/js/main.js
+++ Jesus Flores/week_11/3DJS/js/main.js
@@ -7,8 +7,30 @@ app.init = () => {
         window.innerWidth / window.innerWidth, //Screen aspect ratio
         0.1, //near field: how close to the camera objects can be, before being ignored
         1000,// far field
-        );
+    );  
+    app.camera.position.x = -30;
+    app.camera.position.y = 40;
+    app.camera.position.z = 30;
+    //app.camera.position(-30, 40, 30); 
+    
+    app.camera.lookAt( app.scene.position);
 
+    //The renderer calcukates how to draw all the objects in the scene, based on
+    //the camera prespective and the lighting; it renders it all down to a 
+    //2D image to show in a <canvas>
+
+    app.renderer = new THREE.WebGLRenderer();  //Use hardware acceleration
+    app.renderer.setSize(window.innerWidth, window.innerHeight);
+    app.renderer.setClearColor(0x000000);
+
+    app.renderer.shadowMap.enable = true;
+    app.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    document.getElementById('output').appendChild(app.renderer.domElement);
+
+    app.axes = new THREE.AxesHelper(50);
+    app.scene.add( app.axes ); // Create an axis visualiser and add it to the scene.
+    
+    app.renderer.render( app.scene, app.camera );
 }
 
 
