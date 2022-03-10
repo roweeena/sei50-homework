b816461539a39bde71b9c277d4d6833c43206497
Jesus-fhz <jesusflores9325@gmail.com>
2022-03-10 15:42:38 +1100

    planet and cubes

--- Jesus Flores/week_11/3DJS/js/main-lib.js
+++ Jesus Flores/week_11/3DJS/js/main-lib.js
@@ -1,61 +1,83 @@
-var app = app || {}
+var app = app || {};
+
+console.log('main-lib.js loaded');
 
 app.createPlane = () => {
-    //3D objects are built from two elements 
-    //1. a geometry, aka a shape
-    //2. a material, aka a surface or texture/covering
-    
-    // These are combined into a final 'mesh'
 
-    const planeGeometry = new THREE.PlaneGeometry(120 , 20);
-    const planeMaterial = new THREE.MeshLambertMaterial({
-        color: "OxCCCCCC"
-    })
-    
-    // Combine into a mesh:
-    const plane = new THREE.Mesh(planeGeometry,  planeMaterial);
-    plane.position.set(15, 0, 0); //x y z 
-    plane.rotation.x = -0.5 * Math.PI;
-    plane.receiveShadow = true;
-    
-    return plane
-}
+  // 3d objects are built from two elements:
+  // 1. a geometry, a.k.a. a shape
+  // 2. a material, a.k.a. a surface or texture/covering
+  //
+  // These are combined into a final 'mesh'
 
-//Crate spot light
+  const planeGeometry = new THREE.PlaneGeometry( 120, 20 );
+  const planeMaterial = new THREE.MeshLambertMaterial({
+    color: 0xCCCCCC // grey
+  });
+
+  // Combine into a mesh:
+  const plane = new THREE.Mesh( planeGeometry, planeMaterial  );
+
+  plane.position.set( 15, 0, 0 ); // x, y, z
+  plane.rotation.x = -0.5 * Math.PI;  // because of maths
+  plane.receiveShadow = true; // shadows should be cast onto this
+
+  return plane;
+
+}; // createPlane()
+
+
 app.createSpotlight = () => {
-    const light = new THREE.SpotLight(0xFFFFFF)
-    light.position.set(-10,  60, 10);
-    light.castShadow = true;
-    light.shadow.mapSize.width = 2048;
-    light.shadow.mapSize.width = 2048;   
-    
-    return light
-}
+
+  const light = new THREE.SpotLight( 0xFFFFFF );
+  light.position.set( -10, 60, 10 );
+  light.castShadow = true;
+  light.shadow.mapSize.width = 2048;
+  light.shadow.mapSize.height = 2048;
+  // light.angle = Math.PI/4;
+  // light.distance = 200;
+  return light;
+
+}; // createSpotlight()
+
 
 app.createCube = (width, depth, height) => {
-    const cubeGeometry = new THREE.BoxGeometry(width, depth, height);
-    const cubeMaterial = new THREE.MeshLambertMaterial({
-        color: 0xCF8E00,
-        //wireframe: true
-    })
 
-    const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-    cube.position.set(-4, 15, 0);
-    cube.castShadow = true ;
-    return cube;
-}
+  const cubeGeometry = new THREE.BoxGeometry( width, depth, height );
+  const cubeMaterial = new THREE.MeshLambertMaterial({
+    color: 0xFF8F00,
+    // wireframe: true
+  });
+
+  const cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
+  // cube.position.set( -4, 15, 0 );
+  cube.position.set(
+    THREE.MathUtils.randInt(-50, 50),
+    THREE.MathUtils.randInt(-50, 50),
+    THREE.MathUtils.randInt(-50, 50),
+  );
+  cube.castShadow = true;
+
+  return cube;
+}; // createCube
+
 
 app.createSphere = () => {
-   const sphereGeometry = new THREE.SphereGeometry(
-       6, //radius
-       40,// number of triangle segments on the X axis
-       40,// same for Y axis
-   );
-   const sphereMaterial = new THREE.MeshPhongMaterial({      
-       color: 0x039BE5,
-   })
-   const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-   sphere.position.set(20, 6, 2);
-   sphere.castShadow = true;
-   return sphere;
-}
+
+  const sphereGeometry = new THREE.SphereGeometry(
+    6, // radius
+    40, // number of triangle segments on the X axis
+    40, // same for Y axis
+  );
+
+  const sphereMaterial = new THREE.MeshPhongMaterial({
+    color: 0x039BE5
+  });
+
+  const sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
+  sphere.position.set( 20, 6, 2 );
+  sphere.castShadow = true;
+
+  return sphere;
+
+}; // createSphere()
--- Jesus Flores/week_11/3DJS/js/main.js
+++ Jesus Flores/week_11/3DJS/js/main.js
@@ -1,70 +1,129 @@
-var app = app || {}
+console.log('main.js loaded');
 
-app.init = () => {
-    app.scene = new THREE.Scene();
-    app.camera = new THREE.PerspectiveCamera(
-        60, 
-        window.innerWidth / window.innerWidth, //Screen aspect ratio
-        0.1, //near field: how close to the camera objects can be, before being ignored
-        1000,// far field
-    );  
-    app.camera.position.x = -30;
-    app.camera.position.y = 40;
-    app.camera.position.z = 30;
-    //app.camera.position(-30, 40, 30); 
-    
-    app.camera.lookAt( app.scene.position);
+// use the existing 'app' object (if the other main file loaded first) or, if it's not yet defined, initialise it as an empty object
+var app = app || {};
 
-    //The renderer calcukates how to draw all the objects in the scene, based on
-    //the camera prespective and the lighting; it renders it all down to a 
-    //2D image to show in a <canvas>
-
-    app.renderer = new THREE.WebGLRenderer();  //Use hardware acceleration
-    app.renderer.setSize(window.innerWidth, window.innerHeight);
-    app.renderer.setClearColor(0x000000);
-
-    app.renderer.shadowMap.enabled = true;
-    app.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-    document.getElementById('output').appendChild(app.renderer.domElement);
-
-    app.axes = new THREE.AxesHelper(50  );
-    app.scene.add( app.axes ); // Create an axis visualiser and add it to the scene.
-    
-
-    //Add cube
-
-    app.cube = app.createCube(4,4,4);
-    app.scene.add( app.cube);
-
-    
-    //Add Sphere
-
-    app.sphere = app.createSphere();
-    app.scene.add(app.sphere);
-
-    //let there be light
-    app.spotlight = app.createSpotlight();
-    app.scene.add( app.spotlight )
-
-    //add some cool shit
-    // 1. add a 2D plane i.e sheet, aka 'the runway'
-    app.plane = app.createPlane();
-    app.scene.add( app.plane );
-    app.renderer.render( app.scene, app.camera );
-
-    //Use the mouse to control the camera perspective
-    app.mouseControls = new THREE.OrbitControls( 
-        app.camera,
-        app.renderer.domElement
-    )
-    app.animate();
-
+// ready for dat.gui
+app.controls = {
+  rotationSpeed: 0.01, // to control cube rotation
+  counter: 0,
+  counterIncrement: 0.03 // how fast the counter grows
 };
 
-app.animate = () => {
-   app.renderer.render(app.scene, app.camera); 
-   requestAnimationFrame( app.animate );
-}
- 
+app.init = () => {
+  console.log('init()');
 
-window.onload = app.init();
+  app.scene = new THREE.Scene();
+
+  app.camera = new THREE.PerspectiveCamera(
+    60, // field of view angle
+    window.innerWidth / window.innerHeight, // screen aspect ratio
+    0.1, // near field: how close to the camera objects can be, before being ignored
+    1000 // far field
+  );
+
+  app.camera.position.x = -30;
+  app.camera.position.y = 40;
+  app.camera.position.z = 30;
+  // app.camera.position.set( -30, 40, 30 );
+
+  app.camera.lookAt( app.scene.position ); // Look at 'origin': x=0, y=0, z=0
+
+  // The renderer calculates how to draw all the objects in the scene, based on
+  // the camera perspective and the lighting; it renders it all down to a
+  // 2D image to show in a <canvas>
+  app.renderer = new THREE.WebGLRenderer(); // use hardware acceleration
+  app.renderer.setSize( window.innerWidth, window.innerHeight ); // canvas size
+  app.renderer.setClearColor( 0x000000 ); // background colour
+
+  // Casting shadows is computationally expensive, so we have to opt in:
+  app.renderer.shadowMap.enabled = true;
+  app.renderer.shadowMap.type = THREE.PCFSoftShadowMap; // WTF?
+
+  document.getElementById('output').appendChild( app.renderer.domElement );
+
+
+  app.axes = new THREE.AxesHelper( 50 );
+  app.scene.add( app.axes ); // create an axis visualiser and add it to the scene
+
+
+
+
+  // add some cool shit
+
+  // 1. Add a 2D plane, i.e. a sheet, aka "The Runway"
+  app.plane = app.createPlane();
+  app.scene.add( app.plane ); // add to scene
+
+
+  // 2. Add a cube! A perfect platonic solid
+
+  app.cubes = [];
+
+  for( let i = 0; i < 100; i++ ){
+    const cube =  app.createCube(
+      THREE.MathUtils.randInt(10, 100),
+      4,
+      4
+    );
+    app.scene.add( cube );
+    app.cubes.push( cube );
+  }
+
+  //
+  // app.cube = app.createCube(4, 4, 4);
+  // app.scene.add( app.cube );
+
+  // 3. Add a sphere.... a ball... a planet...
+  // every point on the surface the same distance
+  // from the center... our new home
+  app.sphere = app.createSphere();
+  app.scene.add( app.sphere );
+
+
+  // Let there be light!
+  app.spotlight = app.createSpotlight();
+  app.scene.add( app.spotlight );
+
+  // const spotLightHelper = new THREE.SpotLightHelper( app.spotlight );
+  // app.scene.add( spotLightHelper );
+
+
+  // Use the mouse to control the camera perspective
+  app.mouseControls = new THREE.OrbitControls(
+    app.camera, app.renderer.domElement
+  );
+
+  app.animate(); // start the animation/draw loop
+
+}; // app.init()
+
+
+// animation loop, running at 60 frames/sec iedally
+app.animate = () => {
+
+  app.controls.counter += app.controls.counterIncrement;
+
+  const sphereYOffset = Math.sin( app.controls.counter );
+  // console.log('counter:', app.controls.counter);
+  // console.log('sin offset:', sphereYOffset);
+  app.sphere.position.y = 6 + Math.abs(sphereYOffset * 15);
+
+  const sphereXOffset = Math.cos( app.controls.counter );
+  app.sphere.position.x = 20 + (sphereXOffset * 15)
+
+
+  // app.cube.rotation.x += app.controls.rotationSpeed;
+  // app.cube.rotation.y += app.controls.rotationSpeed*2;
+  // app.cube.rotation.z += app.controls.rotationSpeed;
+
+  app.renderer.render( app.scene, app.camera );
+  requestAnimationFrame( app.animate ); // 60 times/sec
+
+}; // animate()
+
+
+
+
+// oldskool dom readiness handler:
+window.onload = app.init;
